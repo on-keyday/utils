82ad0848de2780f2c1fca24b360688bb548091c5
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-04 20:39:37 +0900

    add test

--- src/test/test_utfconvert.cpp
+++ src/test/test_utfconvert.cpp
@@ -23,6 +23,13 @@ constexpr utils::utf::Minibuffer<char8_t> test_utf32_to_utf8() {
     return result;
 }
 
+constexpr utils::utf::Minibuffer<char16_t> test_utf32_to_utf16() {
+    char32_t testword = U'𠮷';
+    utils::utf::Minibuffer<char16_t> result;
+    utils::utf::utf32_to_utf16(testword, result);
+    return result;
+}
+
 void test_utf_convert() {
     constexpr auto result1 = test_utf8_to_utf32();
     static_assert(result1 == U'𠮷', "utf8_to_utf32 is wrong");
@@ -30,7 +37,10 @@ void test_utf_convert() {
     static_assert(result2 == U'𠮷', "utf16_to_utf32 is wrong");
     constexpr auto expect_result3 = utils::utf::Minibuffer<char8_t>{u8"𠮷"};
     constexpr auto result3 = test_utf32_to_utf8();
-    static_assert(result3 == expect_result3, "utf32_to_utf16 is wrong");
+    static_assert(result3 == expect_result3, "utf32_to_utf8 is wrong");
+    constexpr auto expect_result4 = utils::utf::Minibuffer<char16_t>{u"𠮷"};
+    constexpr auto result4 = test_utf32_to_utf16();
+    static_assert(result4 == expect_result4, "utf32_to_utf16 is wrong");
 }
 
 int main() {
