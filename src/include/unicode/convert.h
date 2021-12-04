1f60fb01c5d11de18b1d269d32087db59eee5c1e
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-04 21:57:31 +0900

    add convert method

--- src/include/unicode/convert.h
+++ src/include/unicode/convert.h
@@ -1,6 +1,6 @@
 /*license*/
 
-// convert - convert method
+// convert - convert() function to use in template
 #pragma once
 
 #include "conv_method.h"
@@ -14,29 +14,44 @@ namespace utils {
         template <class T, class U, size_t expectT, size_t expectU>
         using expect_size_t = expect_size<typename BufferType<T>::char_type, typename BufferType<U>::char_type, expectT, expectU>;
 
-        template <bool decode_all = false, class T, class U, class = expect_size_t<T, U, 1, 4>>
-        constexpr bool convert(Sequencer<T>& in, U& out) {
-            while (!buf.eos()) {
-                char32_t c = 0;
-                if (utf8_to_utf32(in, c)) {
-                    out.push_back(c);
-                }
-                else {
-                    if constexpr (decode_all) {
-                        out.push_back(in.current());
-                        buf.consume();
-                        continue;
-                    }
-                    return false;
-                }
-            }
-            return true;
-        }
+#define DEFINE_CONVERT_WITH_UTF32(FROM, TOMETHOD, FROMMETHOD)                                  \
+    template <bool decode_all = false, class T, class U, class = expect_size_t<T, U, FROM, 4>> \
+    constexpr bool convert(Sequencer<T>& in, U& out) {                                         \
+        while (!in.eos()) {                                                                    \
+            char32_t c = 0;                                                                    \
+            if (TOMETHOD(in, c)) {                                                             \
+                out.push_back(c);                                                              \
+            }                                                                                  \
+            else {                                                                             \
+                if constexpr (decode_all) {                                                    \
+                    out.push_back(in.current());                                               \
+                    buf.consume();                                                             \
+                    continue;                                                                  \
+                }                                                                              \
+                return false;                                                                  \
+            }                                                                                  \
+        }                                                                                      \
+        return true;                                                                           \
+    }                                                                                          \
+                                                                                               \
+    template <bool decode_all = false, class T, class U, class = expect_size_t<T, U, 4, FROM>> \
+    constexpr bool convert(Sequencer<T>& in, U& out) {                                         \
+        while (!in.eos()) {                                                                    \
+            if (!FROMMETHOD(in.current(), out) && !decode_all) {                               \
+                return false;                                                                  \
+            }                                                                                  \
+            in.consume();                                                                      \
+        }                                                                                      \
+        return true;                                                                           \
+    }
 
-        template <bool decode_all = false, class T, class U, class = expect_size_t<T, U, 1, 4>>
+        DEFINE_CONVERT_WITH_UTF32(1, utf8_to_utf32, utf32_to_utf8)
+        DEFINE_CONVERT_WITH_UTF32(2, utf16_to_utf32, utf32_to_utf16)
+
+        template <bool decode_all = false, class T, class U>
         constexpr bool convert(T&& in, U& out) {
             Sequencer<typename BufferType<T&>::type> seq(in);
-            return convert(seq, out);
+            return convert<decode_all>(seq, out);
         }
 
     }  // namespace utf
