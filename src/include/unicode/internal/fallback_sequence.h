cd9714fae2ebeec387e4d20b41d95b8fc0431a5b
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 00:33:03 +0900

    add view

--- src/include/unicode/internal/fallback_sequence.h
+++ src/include/unicode/internal/fallback_sequence.h
@@ -9,20 +9,18 @@
 namespace utils {
     namespace utf {
         namespace internal {
-
             template <class T>
-            bool fallback_impl(Sequencer<T>& seq) {
+            bool fallback(Sequencer<T>& seq) {
                 if (seq.rptr == 0) {
                     return false;
                 }
+                constexpr size_t offset = minbuf_t::bufsize;
                 size_t base = seq.rptr;
-                if (seq.current(-1) < 0x80) {
-                    seq.backto();
-                    return true;
+                if (seq.current(-1) < 0x80 || offset == 1) {
+                    return backto();
                 }
                 using minbuf_t = Minibuffer<typename BufferType<T>::char_type>;
                 minbuf_t minbuf;
-                constexpr size_t offset = minbuf_t::bufsize;
                 for (size_t i = offset; i != 0; i--) {
                     seq.rptr = base - offset;
                     if (convert_one(seq, minbuf)) {
