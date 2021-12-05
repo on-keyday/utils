b4dec29014ea5126a17e218a932ddc50bded4938
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 12:08:20 +0900

    add test

--- src/test/test_utf_operator.cpp
+++ src/test/test_utf_operator.cpp
@@ -5,9 +5,11 @@
 #include <string>
 
 void test_utf_operator() {
-    std::string str;
-    utils::ops::from("") >> str;
+    std::u8string str;
+    utils::ops::from(U"Hello World! こんにちは世界") >> str;
+    assert(str == u8"Hello World! こんにちは世界" && "operator is broken");
 }
 
 int main() {
+    test_utf_operator();
 }
