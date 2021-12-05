4a1ab5b34fd0e2f76ed7d1916aa82a391bd55e9c
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 12:20:40 +0900

    rename

--- src/include/operator/utf_convert.h
+++ src/include/operator/utf_convert.h
@@ -11,23 +11,23 @@ namespace utils {
     namespace ops {
         namespace internal {
             template <class T, bool mask_failure>
-            struct FromObj {
+            struct UtfFromObj {
                 Sequencer<T> seq;
 
                 template <class... Args>
-                constexpr FromObj(Args&&... args)
+                constexpr UtfFromObj(Args&&... args)
                     : seq(std::forward<Args>(args)...) {}
             };
 
             template <class T, bool mask_failure, class U>
-            constexpr bool operator>>(FromObj<T, mask_failure>&& in, U& out) {
+            constexpr bool operator>>(UtfFromObj<T, mask_failure>&& in, U& out) {
                 return utf::convert<mask_failure>(in.seq, out);
             }
         }  // namespace internal
 
         template <bool mask_failure = false, class T>
-        constexpr internal::FromObj<typename BufferType<T&>::type, mask_failure> from(T&& t) {
-            return internal::FromObj<typename BufferType<T&>::type, mask_failure>(std::forward<T>(t));
+        constexpr internal::UtfFromObj<typename BufferType<T&>::type, mask_failure> from(T&& t) {
+            return internal::UtfFromObj<typename BufferType<T&>::type, mask_failure>(std::forward<T>(t));
         }
 
     }  // namespace ops
