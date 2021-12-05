7098f4a7b7828844485df97937d0305e764ab75f
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 14:56:16 +0900

    add endian convert

--- src/include/endian/writer.h
+++ src/include/endian/writer.h
@@ -11,7 +11,6 @@ namespace utils {
         template <class T>
         struct Writer {
             using raw_type = T;
-            using unref_type = std::remove_reference_t<T>;
 
             raw_type buf;
 
@@ -20,7 +19,7 @@ namespace utils {
                 : buf(std::forward<Args>(args)...) {}
 
             template <class T>
-            void write(const T& t) {
+            constexpr void write(const T& t) {
                 const char* ptr = reinterpret_cast<const char*>(&t);
                 for (auto i = 0; i < sizeof(T); i++) {
                     buf.push_back(ptr[i]);
@@ -28,32 +27,33 @@ namespace utils {
             }
 
             template <class T>
-            void write_big(const T& t) {
+            constexpr void write_big(const T& t) {
                 write(to_big(&t));
             }
 
             template <class T>
-            void write_little(const T& t) {
+            constexpr void write_little(const T& t) {
                 write(to_little(&t));
             }
 
             template <class T>
-            void write_hton(const T& t) {
+            constexpr void write_hton(const T& t) {
                 write(to_network(&t));
             }
-#define DEFINE_WRITE_SEQ(FUNC, SUFFIX)          \
-    template <class T>                          \
-    void write_##SUFFIX(const T& seq) {         \
-        for (auto& s : seq) {                   \
-            FUNC(s);                            \
-        }                                       \
-    }                                           \
-                                                \
-    template <class Begin, class End>           \
-    void write_##SUFFIX(Begin begin, End end) { \
-        for (auto i = begin; i != end; i++) {   \
-            FUNC(*i);                           \
-        }                                       \
+
+#define DEFINE_WRITE_SEQ(FUNC, SUFFIX)                    \
+    template <class T>                                    \
+    constexpr void write_##SUFFIX(const T& seq) {         \
+        for (auto& s : seq) {                             \
+            FUNC(s);                                      \
+        }                                                 \
+    }                                                     \
+                                                          \
+    template <class Begin, class End>                     \
+    constexpr void write_##SUFFIX(Begin begin, End end) { \
+        for (auto i = begin; i != end; i++) {             \
+            FUNC(*i);                                     \
+        }                                                 \
     }
             DEFINE_WRITE_SEQ(write, seq)
             DEFINE_WRITE_SEQ(write_big, seq_big)
