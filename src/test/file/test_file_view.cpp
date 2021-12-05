3b769bd1f9cec3e590d8ae25d3f760acc95054d4
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 22:50:59 +0900

    add test

--- src/test/file/test_file_view.cpp
+++ src/test/file/test_file_view.cpp
@@ -3,9 +3,23 @@
 #include "../../include/file/file_view.h"
 #include "../../include/core/sequencer.h"
 
+#include <filesystem>
+
 void test_file_view() {
-    utils::Sequencer<utils::file::View&> view;
+    utils::file::View file;
+    // Assum:
+    // current directry is ${workspaceRoot}/test/
+    // target file ${workspaceRoot}/src/test/file/test.txt is exists
+
+    assert(std::filesystem::exists("../src/test/file/test.txt") && "expected file not exists");
+
+    file.open("../src/test/file/test.txt");
+    assert(file.is_open() && "file not opened");
+    utils::Sequencer<utils::file::View&> view(file);
+
+    assert(view.seek_if(u8"𠮷野家") && "file seek failed");
 }
 
 int main() {
+    test_file_view();
 }
