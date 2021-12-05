1358c9303afff5803d2fb2b67b5e067a166c470e
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 13:02:11 +0900

    add endian convert

--- src/include/endian/endian.h
+++ src/include/endian/endian.h
@@ -8,47 +8,75 @@
 #include <bit>
 #endif
 namespace utils {
+    namespace endian {
+        enum class Endian {
+            big = 0,
+            little = 1,
+            network = big,
+            unknown = 2,
+        };
 
-    enum class Endian {
-        big,
-        little,
-        network = big,
-        unknown,
-    };
-    namespace internal {
-        Endian native_impl() {
-            int base = 1;
-            char* ptr = reinterpret_cast<char*>(&base);
-            if (ptr[0]) {
-                return Endian::little;
+        namespace internal {
+            constexpr Endian native_impl() {
+                int base = 1;
+                const char* ptr = reinterpret_cast<const char*>(&base);
+                if (ptr[0]) {
+                    return Endian::little;
+                }
+                else {
+                    return Endian::big;
+                }
             }
-            else {
-                return Endian::big;
+
+            template <class T, class C>
+            constexpr T copy_as_is(C* in) {
+                static_assert(std::is_integral_v<T> && std::is_integral_v<C>, "T and C must be integer");
+                T result = 0;
+                const char* inptr = reinterpret_cast<const char*>(in);
+                char* outptr = reinterpret_cast<char*>(&result);
+                for (auto i = 0; i < sizeof(T); i++) {
+                    outptr[i] = inptr[i];
+                }
+                return result;
             }
-        }
-    }  // namespace internal
+
+            template <class T, class C>
+            constexpr T reverse_endian(C* in) {
+                static_assert(std::is_integral_v<T> && std::is_integral_v<C>, "T and C must be integer");
+                T result = 0;
+                const char* inptr = reinterpret_cast<const char*>(in);
+                char* outptr = reinterpret_cast<char*>(&result);
+                for (auto i = 0; i < sizeof(T); i++) {
+                    outptr[sizeof(T) - 1 - i] = inptr[i];
+                }
+                return result;
+            }
+        }  // namespace internal
 #ifdef UTILS_USE_STD_ENDIAN
-    consteval Endian native() {
-        return std::endian::native == std::endian::big ? Endian::big : std::endian::native == std::endian::little ? Endian::little
-                                                                                                                  : Endian::unknown;
-    }
+        consteval Endian native() {
+            return std::endian::native == std::endian::big ? Endian::big : std::endian::native == std::endian::little ? Endian::little
+                                                                                                                      : Endian::unknown;
+        }
 #else
-    Endian native() {
-        static Endian native_ = internal::native_impl();
-        return native_;
-    }
+        Endian native() {
+            static Endian native_ = internal::native_impl();
+            return native_;
+        }
 #endif
 
-    template <class T, class C>
-    constexpr T reverse(C* in) {
-        static_assert(std::is_integral_v<T>, "T must be integer");
-        T result = 0;
-        char* inptr = reinterpret_cast<char*>(in);
-        char* outptr = reinterpret_cast<char*>(&result);
-        for (auto i = 0; i < sizeof(T); i++) {
-            outptr[sizeof(T) - 1 - i] = inptr[i];
+        template <class T, class C>
+        constexpr T to_big(C* c) {
+            return native() == Endian::little ? internal::reverse_endian<T>(c) : internal::copy_as_is<T>(c);
         }
-        return result;
-    }
 
+        template <class T, class C>
+        constexpr T to_little(C* c) {
+            return native() == Endian::big ? internal::reverse_endian<T>(c) : internal::copy_as_is<T>(c);
+        }
+
+        template <class T, class C>
+        constexpr T to_network(C* c) {
+            return to_big_endian<T>(c);
+        }
+    }  // namespace endian
 }  // namespace utils
