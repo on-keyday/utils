a1af4d7bee2291f06e3220d31bd8136fed01729b
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 18:44:09 +0900

    add file

--- src/lib/file/platform.cpp
+++ src/lib/file/platform.cpp
@@ -22,6 +22,17 @@ namespace utils {
             static bool open_impl(FileInfo* info, const path_char* path) {
             }
 #endif
+            bool FileInfo::is_open() const {
+                if (this->file || this->fd != ~0
+#ifdef _WIN32
+                    || this->handle
+#endif
+                ) {
+                    return true;
+                }
+                return false;
+            }
+
             bool FileInfo::open(const path_char* path) {
                 if (!path) {
                     return false;
