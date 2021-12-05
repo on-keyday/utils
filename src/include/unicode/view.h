78f46cc23085f6aef3526a522be38b9b30691e70
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 09:57:05 +0900

    add test

--- src/include/unicode/view.h
+++ src/include/unicode/view.h
@@ -16,12 +16,13 @@ namespace utils {
             Minibuffer<char> view;
             size_t viewptr = 0;
             size_t converted_size = 0;
+            size_t virtual_ptr = 0;
 
-            bool fallback() {
+            constexpr bool fallback() {
                 return internal::fallback(sequence);
             }
 
-            bool read_one() {
+            constexpr bool read_one() {
                 if (sequence.eos()) {
                     return false;
                 }
@@ -29,21 +30,54 @@ namespace utils {
                 return convert_one(sequence, view);
             }
 
-            bool count_converted_size(size_t& result) {
+            constexpr bool count_converted_size() {
                 auto tmp = sequence.rptr;
                 sequence.rptr = 0;
                 size_t count = 0;
                 while (!sequence.eos()) {
                     if (!read_one()) {
                         sequence.rptr = tmp;
-                        result = count;
+                        converted_size = 0;
                         return false;
                     }
                     count += view.size();
                 }
-                result = count;
+                converted_size = count;
                 sequence.rptr = tmp;
                 return true;
+            }
+
+            constexpr bool move_to(size_t position) {
+                if (position == virtual_ptr) {
+                    return true;
+                }
+                if (position >= converted_size) {
+                    return false;
+                }
+                if (position > virtual_ptr) {
+                    while (position != virtual_ptr) {
+                        virtual_ptr++;
+                        viewptr++;
+                        if (view.size() >= viewptr) {
+                            if (!read_one()) {
+                                return false;
+                            }
+                            viewptr = 0;
+                        }
+                    }
+                }
+                else {
+                    while (position != virtual_ptr) {
+                        virtual_ptr--;
+                        if (viewptr == 0) {
+                            if (!fallback()) {
+                                return false;
+                            }
+                            viewptr = view.size();
+                        }
+                        viewptr--;
+                    }
+                }
             }
 
            public:
