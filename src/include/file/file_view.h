38e6263375a7727020e54a905c3e940fea2a8f6a
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 21:51:30 +0900

    add file

--- src/include/file/file_view.h
+++ src/include/file/file_view.h
@@ -2,6 +2,7 @@
 
 #include "platform.h"
 #include "../wrap/string.h"
+#include "../utf/convert.h"
 
 namespace utils {
     namespace file {
@@ -10,6 +11,15 @@ namespace utils {
             size_t virtual_ptr = 0;
             bool open(const platform::path_char* path) {
                 return info.open(path);
+            }
+
+            template <class String, class Tmpbuf = wrap::path_string>
+            bool open(String&& str) {
+                Tmpbuf result;
+                if (!utf::convert(str, result)) {
+                    return false;
+                }
+                return open(static_cast<const platform::path_char*>(result.c_str()));
             }
         };
     }  // namespace file
