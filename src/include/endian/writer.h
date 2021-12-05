5b4454fdbe23867fbd559af0c2e4b76c2b8638bd
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 14:50:43 +0900

    add endian convert

--- src/include/endian/writer.h
+++ src/include/endian/writer.h
@@ -28,9 +28,35 @@ namespace utils {
             }
 
             template <class T>
-            void write_hton(const T& t) {
-                return write(to_network(&t));
+            void write_big(const T& t) {
+                write(to_big(&t));
             }
+
+            template <class T>
+            void write_little(const T& t) {
+                write(to_little(&t));
+            }
+
+            template <class T>
+            void write_hton(const T& t) {
+                write(to_network(&t));
+            }
+#define DEFINE_WRITE_SEQ(FUNC, SUFFIX)          \
+    template <class T>                          \
+    void write_##SUFFIX(const T& seq) {         \
+        for (auto& s : seq) {                   \
+            FUNC(s);                            \
+        }                                       \
+    }                                           \
+                                                \
+    template <class Begin, class End>           \
+    void write_##SUFFIX(Begin begin, End end) { \
+        for (auto i = begin; i != end; i++) {   \
+            FUNC(*i);                           \
+        }                                       \
+    }
+            DEFINE_WRITE_SEQ(write, seq)
+            DEFINE_WRITE_SEQ(write_big, seq_big)
         };
     }  // namespace endian
 }  // namespace utils
