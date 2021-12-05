79b1a5b25f5b83ac9328255c6e415b2d175c6dcc
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 14:32:35 +0900

    add endian convert

---
+++ src/include/endian/writer.h
@@ -0,0 +1,31 @@
+/*license*/
+
+// writer - write binary following the endian rule
+#pragma once
+
+#include "endian.h"
+
+namespace utils {
+    namespace endian {
+
+        template <class T>
+        struct Writer {
+            using raw_type = T;
+            using unref_type = std::remove_reference_t<T>;
+
+            raw_type buf;
+
+            template <class... Args>
+            Writer(Args&&... args)
+                : buf(std::forward<Args>(args)...) {}
+
+            template <class T>
+            void write(const T& t) {
+                const char* ptr = reinterpret_cast<const char*>(&t);
+                for (auto i = 0; i < sizeof(T); i++) {
+                    buf.push_back(ptr[i]);
+                }
+            }
+        };
+    }  // namespace endian
+}  // namespace utils
