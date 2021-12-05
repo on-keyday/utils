3a6b1ce9c45a28032532d6579df45b2584ec1413
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-05 18:37:07 +0900

    add file

--- src/lib/file/platform.cpp
+++ src/lib/file/platform.cpp
@@ -1,5 +1,32 @@
 /*license*/
 
 #include "../../include/file/platform.h"
+#include <windows.h>
+using namespace utils::file::platform;
 
-using namespace utils::file;
+namespace utils {
+    namespace file {
+        namespace platform {
+#ifdef _WIN32
+            static bool open_impl(FileInfo* info, const path_char* path) {
+                ::CreateFileW(
+                    path,
+                    GENERIC_READ,
+                    FILE_SHARE_READ,
+                    NULL,
+                    OPEN_EXISTING,
+                    FILE_ATTRIBUTE_NORMAL,
+                    NULL);
+            }
+#else
+            static bool open_impl(FileInfo* info, const path_char* path) {
+            }
+#endif
+            bool FileInfo::open(const path_char* path) {
+                if (!path) {
+                    return false;
+                }
+            }
+        }  // namespace platform
+    }      // namespace file
+}  // namespace utils
