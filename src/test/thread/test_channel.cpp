6ab94295b40ba2eb3481c15dd5b6811d6151a086
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-06 19:29:43 +0900

    add channel

---
+++ src/test/thread/test_channel.cpp
@@ -0,0 +1,22 @@
+/*license*/
+
+#include "../../include/thread/channel.h"
+#include <cassert>
+
+void test_channecl() {
+    auto [w, r] = utils::thread::make_chan<int>(5);
+    w << 32;
+    int result;
+    r >> result;
+    assert(result == 32 && "channel is incorrect");
+    w << 1;
+    w << 2;
+    w << 3;
+    w << 4;
+    w << 5;
+    auto result2 = w << 6;
+    assert(result2 == utils::thread::ChanStateValue::full && "expect full but not");
+}
+
+int main() {
+}
