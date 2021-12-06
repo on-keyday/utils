c11e4a1658a7aca8ebb9c6bfbc736a582a579e7e
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-06 18:24:31 +0900

    add channel

--- src/include/thread/channel.h
+++ src/include/thread/channel.h
@@ -3,10 +3,49 @@
 // channel - channel between thread
 #pragma once
 
+#include "lite_lock.h"
+#include "../wrap/lite/queue.h"
+#include "../wrap/lite/enum.h"
+
 namespace utils {
     namespace thread {
 
-        struct Buffer {
+        enum class ChanStateValue {
+            enable,
+            closed,
+            empty,
+            full,
+        };
+
+        enum class ChanDisposePolicy {
+            dispose_new,
+            dispose_back,
+            dispose_front,
+        };
+
+        using ChanState = wrap::EnumWrap<ChanStateValue, ChanStateValue::enable, ChanStateValue::closed>;
+
+        template <class T, template <class...> class Que = wrap::queue>
+        struct ChanBuffer {
+            Que<T> que;
+            size_t limit = ~0;
+            LiteLock lock_;
+            ChanDisposePolicy policy = ChanDisposePolicy::dispose_new;
+
+            ChanState store(T&& t) {
+                lock_.lock();
+                if (que.size() >= limit) {
+                    if (policy == ChanDisposePolicy::dispose_front) {
+                        que.pop_front();
+                    }
+                    else if (policy == ChanDisposePolicy::dispose_back) {
+                        que.pop_back();
+                    }
+                    else {
+                        return ChanStateValue::full;
+                    }
+                }
+            }
         };
     }  // namespace thread
 }  // namespace utils
