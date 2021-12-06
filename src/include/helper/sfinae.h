4c0d75009d4c88fca22704ab1d867704ed1982f4
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-06 12:14:00 +0900

    add sfinae

--- src/include/helper/sfinae.h
+++ src/include/helper/sfinae.h
@@ -16,5 +16,20 @@ namespace utils {
 #define DEFINE_SFINAE_T(NAME, EXPR) SFINAE_HELPER_BASE(NAME, <class T>, <T>, EXPR)
 #define DEFINE_SFINAE_TU(NAME, EXPR) SFINAE_HELPER_BASE(NAME, <class T, class U>, <T, U>, EXPR)
 
+#define SFINAE_BLOCK_BEGIN_BASE(NAME, PARAM, ARG, EXPR, ...)   \
+    SFINAE_HELPER_BASE(NAME##_flag, PARAM, ARG, EXPR);         \
+    template <__VA_ARGS__, bool flag = NAME##_flag ARG::value> \
+    struct NAME {
+#define SFINAE_BLOCK_ELSE_BASE(NAME, PARAM, ARG) \
+    }                                            \
+    ;                                            \
+    template PARAM struct NAME ARG {
+#define SFINAE_BLOCCK_END_BASE() \
+    }                            \
+    ;
+
+#define SFINAE_BLOCK_T_BEGIN(NAME, EXPR) SFINAE_BLOCK_BEGIN_BASE(NAME, <class T>, <T>, EXPR, class T)
+#define SFINAE_BLOCK_T_ELSE(NAME) SFINAE_BLOCK_ELSE_BASE(NAME, <class T>, <T, false>)
+#define SFINAE_BLOCK_T_END() SFINAE_BLOCCK_END_BASE()
     }  // namespace helper
 }  // namespace utils
--- src/include/wrap/cout.h
+++ src/include/wrap/cout.h
@@ -9,6 +9,7 @@
 #include "lite/string.h"
 #include "../helper/sfinae.h"
 #include "../core/buffer.h"
+#include "../utf/convert.h"
 
 namespace utils {
     namespace wrap {
@@ -23,6 +24,18 @@ namespace utils {
             std::stringstream ss;
 
             DEFINE_SFINAE_T(is_string, std::declval<Buffer<typename BufferType<T&>::type>>());
+
+            template <class T, bool flag = is_string<T>::value>
+            UtfOut& operator<<(T&& t) {
+                wrap::path_string tmp;
+                utf::convert<true>(t, tmp);
+                write(tmp.c_str());
+                return *this;
+            }
+
+            template <class T>
+            UtfOut& operator<<(T&& t) {
+            }
 
             void write(const path_char* str);
         };
