e9ed5bf3e2c53543f09b14f6ce58069fd1477d96
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-06 20:56:47 +0900

    add fork

--- src/include/thread/fork_channel.h
+++ src/include/thread/fork_channel.h
@@ -4,11 +4,32 @@
 #pragma once
 
 #include "lite_lock.h"
+#include "../wrap/lite/map.h"
+#include "channel.h"
+#include <algorithm>
 
 namespace utils {
     namespace thread {
+
+        template <class T, template <class...> class Que = wrap::queue, template <class...> class Map = wrap::map>
         struct ForkBuffer {
             LiteLock lock_;
+            Map<size_t, SendChan<T, Que>> litenr;
+            void store(T&& t) {
+                lock_.lock();
+                T copy(std::move(t));
+                std::erase_if(listner.begin(), litenr.end(), [&](auto& v) {
+                    SendChan<T, Que>& c = std::get<1>(v);
+                    if (c.is_closed()) {
+                        return true;
+                    }
+                    auto tomov = copy;
+                    if ((c << std::move(tomov)) == ChanStateValue::closed) {
+                        return true;
+                    }
+                    return false;
+                });
+            }
         };
     }  // namespace thread
 }  // namespace utils
