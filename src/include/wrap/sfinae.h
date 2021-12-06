05609eea4740ef7438dbdfd00aa3f02fb881dfa1
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-06 11:40:32 +0900

    sfinae

--- src/include/wrap/sfinae.h
+++ src/include/wrap/sfinae.h
@@ -6,12 +6,14 @@
 
 namespace utils {
     namespace wrap {
-#define sFINAE_HELPER_BASE(NAME, PARAM, ARG, EXPR)                            \
+#define SFINAE_HELPER_BASE(NAME, PARAM, ARG, EXPR)                            \
     struct NAME##_impl {                                                      \
         template PARAM static std::true_type has(decltype((EXPR), (void)0)*); \
         template PARAM static std::false_type has(...);                       \
     };                                                                        \
     template PARAM struct NAME : decltype(NAME##_impl::template has ARG(nullptr)) {}
+
+#define DEFINE_SFINAE_T(NAME, EXPR) SFINAE_HELPER_BASE(NAME, <class T>, <T>, EXPR)
 
     }  // namespace wrap
 }  // namespace utils
