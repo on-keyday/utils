1fc2376d0ae782507bdecd620cc1d8e03b7ba861
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-06 21:00:30 +0900

    add fork

--- src/include/thread/channel.h
+++ src/include/thread/channel.h
@@ -38,7 +38,7 @@ namespace utils {
             ChanDisposePolicy policy = ChanDisposePolicy::dispose_new;
 
             bool check_limit() {
-                while (que.size() >= limit) {
+                while (que.size() >= limit && que.size() != 0) {
                     if (policy == ChanDisposePolicy::dispose_front) {
                         que.pop_front();
                     }
