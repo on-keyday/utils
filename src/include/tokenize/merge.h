4ef3de5dcce4df52c05fc9d45782535d31e4eefb
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-07 18:47:34 +0900

    add tokenizer test

--- src/include/tokenize/merge.h
+++ src/include/tokenize/merge.h
@@ -8,17 +8,18 @@
 
 namespace utils {
     namespace tokenize {
-        enum class CommentType {
-            block = 0x1,
-            oneline = 0x2,
-            escape = 0x4,
-            nest = 0x10,
-            allow_line = 0x20,
-        };
-
-        DEFINE_ENUM_FLAGOP(CommentType)
 
         namespace internal {
+            enum class CommentType {
+                block = 0x1,
+                oneline = 0x2,
+                escape = 0x4,
+                nest = 0x10,
+                allow_line = 0x20,
+            };
+
+            DEFINE_ENUM_FLAGOP(CommentType)
+
             template <class String>
             struct CommentMergeContext {
                 CommentType type;
@@ -28,29 +29,17 @@ namespace utils {
             };
 
             template <class String>
-            bool merge_impl(wrap::shared_ptr<Token<String>>& inout, CommentMergeContext<String>& ctx) {
+            int merge_impl(wrap::shared_ptr<Token<String>>& inout, CommentMergeContext<String>& ctx) {
                 if (any(ctx.type & CommentType::block)) {
-                    auto res = make_comment_between(inout, ctx.begin, ctx.end, any(ctx.type & CommentType::nest));
-                    if (res < 0) {
-                        return false;
-                    }
+                    return make_comment_between(inout, ctx.begin, ctx.end, any(ctx.type & CommentType::nest));
                 }
                 else if (any(ctx.type & CommentType::oneline)) {
-                    auto res = make_comment_util_line(inout, ctx.begin);
-                    if (res < 0) {
-                        return false;
-                    }
+                    return make_comment_util_line(inout, ctx.begin);
                 }
                 else if (any(ctx.type & CommentType::escape)) {
-                    auto res = make_comment_with_escape(inout, ctx.begin, ctx.end, ctx.escape, any(ctx.type & CommentType::allow_line));
-                    if (res < 0) {
-                        return false;
-                    }
+                    return make_comment_with_escape(inout, ctx.begin, ctx.end, ctx.escape, any(ctx.type & CommentType::allow_line));
                 }
-                else {
-                    return false;
-                }
-                return true;
+                return -1;
             }
 
             template <class String>
@@ -61,10 +50,10 @@ namespace utils {
             template <class String, class Ctx, class... Ctxs>
             bool merge_each(wrap::shared_ptr<Token<String>>& inout, Ctx&& ctx, Ctxs&&... other) {
                 auto res = merge_impl(inout, ctx);
-                if (!res) {
+                if (res < 0) {
                     return false;
                 }
-                if (!inout) {
+                if (res == 1) {
                     return true;
                 }
                 return merge_each(inout, std::forward<Ctxs>(other)...);
@@ -72,7 +61,37 @@ namespace utils {
         }  // namespace internal
 
         template <class String>
-        bool merge(wrap::shared_ptr<Token<String>>& input) {
+        internal::CommentMergeContext<String> blcok_comment(const String& begin, const String& end, bool nest = false) {
+            internal::CommentType type = internal::CommentType::block;
+            if (nest) {
+                type |= internal::CommentType::nest;
+            }
+            return internal::CommentMergeContext<String>{
+                .type = type,
+                .begin = begin,
+                .end = end,
+            };
+        }
+
+        template <class String>
+        internal::CommentMergeContext<String> line_comment(const String& begin) {
+            return internal::CommentMergeContext<String>{
+                .type = internal::CommentType::oneline,
+                .begin = begin,
+            };
+        }
+
+        template <class String, class... Ctx>
+        bool merge(wrap::shared_ptr<Token<String>>& input, Ctx&&... ctx) {
+            for (auto inout = input; inout; inout = inout->next) {
+                if (!internal::merge_each(inout, std::forward<Ctx>(ctx)...)) {
+                    return true;
+                }
+                if (!inout) {
+                    break;
+                }
+            }
+            return true;
         }
 
     }  // namespace tokenize
