955bd36362b19a15f4b1478acbef83a48dc07cc5
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-07 22:34:06 +0900

    add syntax

--- src/include/syntax/parse.h
+++ src/include/syntax/parse.h
@@ -9,7 +9,25 @@ namespace utils {
     namespace syntax {
 
         template <class String, template <class...> class Vec>
+        bool parse_single(Reader<String>& r, wrap::shared_ptr<Element<String, Vec>>& single) {
+            auto e = r.read();
+            if (e->has("\"")) {
+                r.conusme();
+                e = r.get();
+                if (!e->is(tknz::TokenKind::comment)) {
+                    return false;
+                }
+            }
+        }
+
+        template <class String, template <class...> class Vec>
         bool parse_group(Reader<String>& r, wrap::shared_ptr<Element<String, Vec>>& group) {
+            auto gr = wrap::make_shared<Group<String, Vec>>();
+            gr->type = SyntaxType::group;
+            auto e = r.read();
+            if (!e) {
+                return false;
+            }
         }
     }  // namespace syntax
 }  // namespace utils
