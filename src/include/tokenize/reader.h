0965d9743e7b2542f5f94f0f21a48dcd91d7c1ba
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-07 21:11:59 +0900

    add tokenizer test

--- src/include/tokenize/reader.h
+++ src/include/tokenize/reader.h
@@ -9,12 +9,61 @@ namespace utils {
     namespace tokenize {
         template <class String>
         struct Reader {
-            wrap::shared_ptr<Token<String>> root;
-            wrap::shared_ptr<Token<String>> current;
+            using token_t = wrap::shared_ptr<Token<String>>;
+            token_t root;
+            token_t current;
 
             constexpr Reader() {}
-            constexpr Reader(wrap::shared_ptr<Token<String>>& v)
+            constexpr Reader(token_t& v)
                 : root(v), current(v) {}
+
+            void seek_root() {
+                root = current;
+            }
+
+           protected:
+            static bool default_ignore(token_t& tok) {
+                return tok->is(TokenKind::root) ||
+                       tok->is(TokenKind::space) ||
+                       tok->is(TokenKind::line) ||
+                       tok->is(TokenKind::unknown) ||
+                       tok->is(TokenKind::comment);
+            }
+
+            virtual bool is_ignore() {
+                return default_ignore(current);
+            }
+
+            virtual void consume_hook() {}
+
+            void consume_impl() {
+                current = current->next;
+                consume_hook();
+            }
+
+           public:
+            bool consume() {
+                if (!current) {
+                    return false;
+                }
+                consume_impl();
+                return true;
+            }
+
+            token_t& read() {
+                if (current && is_ignore()) {
+                    consume_impl();
+                }
+                return current;
+            }
+
+            token_t& get() {
+                return current;
+            }
+
+            bool eos() const {
+                return current == nullptr;
+            }
         };
     }  // namespace tokenize
 }  // namespace utils
