6324ee6ab68496fda32130464309338986928b65
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-07 16:43:55 +0900

    add tokenizer test

--- src/include/tokenize/comment.h
+++ src/include/tokenize/comment.h
@@ -8,19 +8,46 @@ namespace utils {
     namespace tokenize {
 
         template <bool nest, class String>
-        bool merge_until(String& merged, wrap::shared_ptr<Token<String>>& root, wrap::shared_ptr<Token<String>>& lastmerged, const String& begin, const String& end) {
+        bool merge_until(String& merged, wrap::shared_ptr<Token<String>>& root, wrap::shared_ptr<Token<String>>& lastmerged, wrap::shared_ptr<Token<String>>& lastp, const String& begin, const String& end) {
             auto prev = root;
-
-            for (auto p = root; p; p = p->next) {
+            auto kind = root->kind;
+            auto p = root->next;
+            size_t count = 0;
+            for (; p; p = p->next) {
+                if constexpr (nest) {
+                    if (p->is(kind) && p->has(begin)) {
+                        count++;
+                    }
+                }
+                if ((p->is(TokenKind::symbol) || p->is(TokenKind::keyword)) && p->has(end)) {
+                    if (count == 0) {
+                        lastmerged = prev;
+                        lastp = p;
+                        return true;
+                    }
+                    count--;
+                }
+                merged += p->to_string();
+                prev = p;
             }
+            return false;
         }
 
         template <bool nest, class String>
-        bool make_comment_between(wrap::shared_ptr<Token<String>>& p, const String& begin, const String& end) {
+        int make_comment_between(String& merged, wrap::shared_ptr<Token<String>>& p, const String& begin, const String& end) {
             if (p->is(TokenKind::symbol) || p->is(TokenKind::keyword)) {
                 if (p->has(begin)) {
+                    wrap::shared_ptr<Token<String>> lastmerged, lastp;
+                    if (!merge_until(merged, p, lastmerged, lastp, begin, end)) {
+                        return -1;
+                    }
+                    if (p->next == lastp) {
+                        p = lastp;
+                        return 1;
+                    }
                 }
             }
+            return 0;
         }
 
         template <class String>
