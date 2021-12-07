ab86e732aa2bdb9071398529286c84d17ba2a26f
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-07 17:23:18 +0900

    add tokenizer test

--- src/include/tokenize/comment.h
+++ src/include/tokenize/comment.h
@@ -23,65 +23,67 @@ namespace utils {
                 return comment == str;
             }
         };
-
-        template <class String, class F>
-        bool merge_until(String& merged, wrap::shared_ptr<Token<String>>& root, F&& f) {
-            auto p = root->next;
-            size_t count = 0;
-            for (; p; p = p->next) {
-                if (f(p)) {
-                    return true;
+        namespace internal {
+            template <class String, class F>
+            bool merge_until(String& merged, wrap::shared_ptr<Token<String>>& root, F&& f) {
+                auto p = root->next;
+                size_t count = 0;
+                for (; p; p = p->next) {
+                    if (f(p)) {
+                        return true;
+                    }
+                    merged += p->to_string();
                 }
-                merged += p->to_string();
+                return false;
             }
-            return false;
-        }
+
+            template <class String>
+            bool is_symbol_or_keyword_and_(wrap::shared_ptr<Token<String>>& p, const String& str) {
+                return (p->is(TokenKind::symbol) || p->is(TokenKind::keyword)) && p->has(str);
+            }
+        }  // namespace internal
 
         template <bool nest, class String>
         int make_comment_between(wrap::shared_ptr<Token<String>>& p, const String& begin, const String& end) {
-            if (p->is(TokenKind::symbol) || p->is(TokenKind::keyword)) {
-                if (p->has(begin)) {
-                    wrap::shared_ptr<Token<String>> lastp;
-                    String merged;
-                    size_t count = 0;
-                    auto kind = p->kind();
-                    auto rule = [&](auto& p) {
-                        if constexpr (next) {
-                            if (p->is(kind) && p->has(begin)) {
-                                count++;
-                            }
+            if (internal::is_symbol_or_keyword_and_(p, begin)) {
+                wrap::shared_ptr<Token<String>> lastp;
+                String merged;
+                size_t count = 0;
+                auto kind = p->kind();
+                auto rule = [&](auto& p) {
+                    if constexpr (next) {
+                        if (p->is(kind) && p->has(begin)) {
+                            count++;
                         }
-                        if (p->is(TokenKind::symbol) || p->is(TokenKind::keyword)) {
-                            if (p->has(end)) {
-                                if (count == 0) {
-                                    lastp = p;
-                                    return true;
-                                }
-                                count--;
-                            }
+                    }
+                    if (internal::is_symbol_or_keyword_and_(p, end)) {
+                        if (count == 0) {
+                            lastp = p;
+                            return true;
                         }
-                        return false;
-                    };
-                    if (!merge_until(merged, p, rule)) {
-                        return -1;
+                        count--;
                     }
-                    if (p->next == lastp) {
-                        p = lastp;
-                        return 1;
-                    }
-                    auto comment = = wrap::make_shared<Comment<String>>();
-                    comment->comment = std::move(merged);
-                    p->next = comment;
-                    comment->next = lastp;
+                    return false;
+                };
+                if (!internal::merge_until(merged, p, rule)) {
+                    return -1;
+                }
+                if (p->next == lastp) {
                     p = lastp;
                     return 1;
                 }
+                auto comment = = wrap::make_shared<Comment<String>>();
+                comment->comment = std::move(merged);
+                p->next = comment;
+                comment->next = lastp;
+                p = lastp;
+                return 1;
             }
             return 0;
         }
 
-        template <bool nest, class String>
-        int make_comment_between(String& merged, wrap::shared_ptr<Token<String>>& p, const String& begin) {
+        template <class String>
+        int make_comment_util_line(String& merged, wrap::shared_ptr<Token<String>>& p, const String& begin) {
         }
 
     }  // namespace tokenize
