2e48d7b310ca095f78369eff5c383d9c5eb18d2e
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-07 14:37:35 +0900

    add tokenizer

--- src/test/tokenize/test_tokenizer.cpp
+++ src/test/tokenize/test_tokenizer.cpp
@@ -7,4 +7,18 @@ using namespace utils;
 
 void test_tokenizer() {
     tokenize::Tokenizer<wrap::string> tokenizer;
+    decltype(tokenizer)::token_t token;
+
+    tokenizer.keyword.predef = {"def", "func", "int", "bool"};
+    tokenizer.symbol.predef = {"(", ")", "->"};
+
+    char8_t testword[] =
+        u8R"(
+        def あれれ int
+        func Hey()->bool
+    )";
+
+    Sequencer input(testword);
+
+    tokenizer.tokenize(input, token);
 }
