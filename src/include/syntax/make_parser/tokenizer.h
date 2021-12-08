33037109a554fb54b4029588cf759fc3cbcab367
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-08 13:05:56 +0900

    add syntax

--- src/include/syntax/make_parser/tokenizer.h
+++ src/include/syntax/make_parser/tokenizer.h
@@ -1,6 +1,8 @@
 /*license*/
 
 #include "../../tokenize/tokenizer.h"
+#include "keyword.h"
+#include "attribute.h"
 
 namespace utils {
     namespace syntax {
@@ -8,7 +10,9 @@ namespace utils {
 
         template <class String, template <class...> class Vec = wrap::vector>
         tknz::Tokenizer<String, Vec> make_tokenizer() {
-            tknz::Tokenizer<String, Vec>
+            tknz::Tokenizer<String, Vec> ret;
+            for (auto i = 0; i < sizeof(keyword_str) / sizeof(keyword_str[0]); i++) {
+            }
         }
     }  // namespace syntax
 }  // namespace utils
