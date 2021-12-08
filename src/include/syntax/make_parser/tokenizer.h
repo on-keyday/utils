d8d3ec9116a4261546636ae1b8025dc4a2e4f55f
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-08 13:14:49 +0900

    add syntax

--- src/include/syntax/make_parser/tokenizer.h
+++ src/include/syntax/make_parser/tokenizer.h
@@ -11,11 +11,24 @@ namespace utils {
         template <class String, template <class...> class Vec = wrap::vector>
         tknz::Tokenizer<String, Vec> make_tokenizer() {
             tknz::Tokenizer<String, Vec> ret;
+            auto cvt_push = [&](auto base, auto& predef) {
+                String converted;
+                utf::convert(base, converted);
+                predef.push_back(std::move(converted));
+            };
             for (auto i = 0; i < sizeof(keyword_str) / sizeof(keyword_str[0]); i++) {
-                String keyword;
-                utf::convert(keyword_str[i], keyword);
-                ret.keyword.predef.push_back(std::move(keyword));
+                cvt_push(keyword_str[i], ret.keyword.predef);
             }
+            cvt_push("[", ret.symbol.predef);
+            cvt_push("]", ret.symbol.predef);
+            cvt_push("|", ret.symbol.predef);
+            cvt_push("#", ret.symbol.predef);
+            cvt_push("\"", ret.symbol.predef);
+            cvt_push(attribute(Attribute::adjacent), ret.symbol.predef);
+            cvt_push(attribute(Attribute::fatal), ret.symbol.predef);
+            cvt_push(attribute(Attribute::ifexists), ret.symbol.predef);
+            cvt_push(attribute(Attribute::repeat), ret.symbol.predef);
+            return ret;
         }
     }  // namespace syntax
 }  // namespace utils
