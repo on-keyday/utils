3b814dfaba973e515247435dc7b5a19fe366cebe
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-10 20:57:14 +0900

    add match

--- src/include/syntax/matching/keyword_literal.h
+++ src/include/syntax/matching/keyword_literal.h
@@ -1,6 +1,6 @@
 /*license*/
 
-// keyword -matching keyword
+// keyword_literal -matching keyword and literal
 #pragma once
 
 #include "context.h"
@@ -176,6 +176,34 @@ namespace utils {
                     report("unsupported keyword `", v->tok->to_string(), "`");
                     return MatchState::fatal;
                 }
+            }
+
+            template <class String, template <class...> class Vec>
+            MatchState match_literal(Context<String, Vec>& ctx, wrap::shared_ptr<Element<String, Vec>>& v, MatchResult<String>& result) {
+                auto report = [&](auto&&... args) {
+                    ctx.err.packln("error: ", args...);
+                    ctx.errat = r.get();
+                    ctx.errelement = v;
+                };
+                Single<String, Vec>* value = static_cast<Single<String, Vec>*>(std::addressof(*v));
+                assert(value);
+                Reader<String>& r = ctx.r;
+                if (!any(value->attr & Attribute::adjacent)) {
+                    r.read();
+                }
+                auto e = r.get();
+                if (!e) {
+                    report("unexpected EOF");
+                    return MatchState::not_match;
+                }
+                if (!e->is(tknz::TokenKind::keyword) && !e->is(tknz::TokenKind::symbol)) {
+                    report("expect keyword but token is ", e->what(), " (symbol `", e->to_string(), "`)");
+                    return MatchState::not_match;
+                }
+                r.consume();
+                result.kind = e->is(tknz::TokenKind::keyword) ? KeyWord::literal_keyword : KeyWord::literal_symbol;
+                result.token = e->to_string();
+                return MatchState::succeed;
             }
         }  // namespace internal
     }      // namespace syntax
---
+++ src/include/syntax/matching/matching.h
@@ -0,0 +1,14 @@
+/*license*/
+
+// matching - do matching
+#pragma once
+
+#include "keyword_literal.h"
+#include "group_ref_or.h"
+
+namespace utils {
+    namespace syntax {
+        struct Match {
+        };
+    }  // namespace syntax
+}  // namespace utils
