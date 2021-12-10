1dfb98e95491d6eec10b871c492fb8f4bb4e319d
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-10 14:43:24 +0900

    add match

---
+++ src/include/syntax/matching/ref_or.h
@@ -0,0 +1,14 @@
+/*license*/
+
+// ref_or - matching `or` element and `reference` element
+#pragma once
+#include "context.h"
+#include "state.h"
+
+namespace utils {
+    namespace syntax {
+        template <class String, template <class...> class Vec>
+        MatchState match_or(Context<String, Vec>& ctx, wrap::shared_ptr<Element<String, Vec>>& v) {
+        }
+    }  // namespace syntax
+}  // namespace utils
