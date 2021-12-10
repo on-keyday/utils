c3fe7b97144451cb3df83c477981d2d9b8bb190f
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-10 23:50:52 +0900

    add match

--- src/include/syntax/matching/number.h
+++ src/include/syntax/matching/number.h
@@ -189,10 +189,10 @@ namespace utils {
                         return -1;
                     }
                     if (pt.afterdot) {
-                        r.current = pt.afterdot->next;
+                        ctx.r.current = pt.afterdot->next;
                     }
                     else if (pt.dot) {
-                        r.current = pt.dot->next;
+                        ctx.r.current = pt.dot->next;
                     }
                     else {
                         report("parser is broken");
