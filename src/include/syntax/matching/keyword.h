eecc3101e8d5e21edaea4194854d9eb59f1389dd
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-10 11:12:42 +0900

    add match

--- src/include/syntax/matching/keyword.h
+++ src/include/syntax/matching/keyword.h
@@ -6,6 +6,7 @@
 #include "context.h"
 #include "../make_parser/keyword.h"
 #include "state.h"
+#include "number.h"
 
 namespace utils {
     namespace syntax {
@@ -17,17 +18,27 @@ namespace utils {
                 ctx.errat = r.get();
                 ctx.errelement = v;
             };
-            Reader<String>& r = ctx.r;
-            if (!any(v->attr & Attribute::adjacent)) {
-                r.read();
-            }
             Single<String, Vec>* value = static_cast<Single<String, Vec>*>(std::addressof(*v));
+            Reader<String>& r = ctx.r;
+            auto is_keyword = [&](KeyWord kwd) {
+                return value->tok->has(keywordv(kwd));
+            };
+            if (!any(v->attr & Attribute::adjacent)) {
+                if (is_keyword(KeyWord::eol)) {
+                    r.ignore_line = false;
+                    r.read();
+                    r.ignore_line = true;
+                }
+                else {
+                    r.read();
+                }
+            }
             auto e = r.get();
             MatchState ret = MatchState::not_match;
             auto common_begin = [&](KeyWord kwd, auto&& f) {
-                if (value->tok->has(keywordv(kwd))) {
+                if (is_keyword(kwd)) {
                     if (!e) {
-                        report("unexpected EOF. expect identifier.");
+                        report("unexpected EOF");
                         ret = MatchState::eof;
                         return true;
                     }
@@ -35,14 +46,24 @@ namespace utils {
                     return true;
                 }
             };
-            if (common_begin(KeyWord::id, [&] {
-                    if (!e->is(tknz::TokenKind::identifier)) {
-                        return false;
+            auto filter_tokenkind = [&](tknz::TokenKind kind) {
+                return [&, kind] {
+                    if (!e->is(kind)) {
+                        report("expect ", tknz::token_kind(kind), " but token is ", e->what(), "(symbol `", e->to_string(), "`)");
+                        return;
                     }
                     token = e->to_string();
                     ret = MatchState::succeed;
                     r.consume();
-                })) {
+                };
+            };
+            if (common_begin(KeyWord::id, filter_tokenkind(tknz::TokenKind::identifier))) {
+                return ret;
+            }
+            else if (common_begin(KeyWord::keyword, filter_tokenkind(tknz::TokenKind::keyword))) {
+                return ret;
+            }
+            else if (common_begin(KeyWord::symbol, filter_tokenkind(tknz::TokenKind::symbol))) {
                 return ret;
             }
         }
