db6a3b030c5963fe1fae0bc29aabd510f7f68dd7
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-11 14:28:41 +0900

    refactoring

--- src/lib/syntax/default_syntaxc.cpp
+++ src/lib/syntax/default_syntaxc.cpp
@@ -7,6 +7,22 @@ namespace utils {
     namespace syntax {
         void instanciate() {
             SyntaxC<wrap::string, wrap::vector, wrap::map> instance;
+            utils::Sequencer input("");
+
+            utils::tokenize::Tokenizer<utils::wrap::string, utils::wrap::vector> tokenizer;
+
+            instance.make_tokenizer(input, tokenizer);
+
+            auto other = "";
+
+            utils::Sequencer input2(other);
+
+            decltype(tokenizer)::token_t res;
+
+            tokenizer.tokenize(input2, res);
+
+            utils::syntax::Reader<utils::wrap::string> r{res};
+            instance.matching(r);
         }
     }  // namespace syntax
 }  // namespace utils
