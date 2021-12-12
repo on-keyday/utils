c18cf74514ea4aaf4eda45fed4b16ffd2a7ab8ea
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-12 12:03:42 +0900

    add bin fmt

--- src/include/bin_fmt/token/token_list.h
+++ src/include/bin_fmt/token/token_list.h
@@ -6,19 +6,33 @@
 #include "../../tokenize/token.h"
 #include "../../tokenize/cast.h"
 #include "../../endian/writer.h"
+#include "../numstr.h"
+
+#include <cassert>
 
 namespace utils {
     namespace bin_fmt {
         template <class Buf, class String>
-        bool write_token(endian::Writer<Buf>& w, const tokenize::Token<String>& in) {
+        bool write_token(endian::Writer<Buf>& w, tokenize::Token<String>& in) {
             if (!in) {
                 return false;
             }
-            using tokenize::TokenKind;
+            using namespace tokenize;
             TokenKind kind = in.kind();
             w.write_hton(std::uint8_t(kind));
             if (kind == TokenKind::root || kind == TokenKind::unknown) {
                 return true;
+            }
+            else if (kind == TokenKind::identifier) {
+                Identifier<String>* id = cast<Identifier>(&in);
+                assert(id);
+                return BinaryIO::write_string(w, id->identifier);
+            }
+            else if (kind == TokenKind::line) {
+                Line<String>* line = cast<Line>(&in);
+                assert(line);
+                return BinaryIO::write_num(w, size_t(line->line)) &&
+                       BinaryIO::write_num(w, size_t(line->count));
             }
         }
     }  // namespace bin_fmt
