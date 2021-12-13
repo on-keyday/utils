086a1c8e238ac6b9af3dc55952e382e36905dd26
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-13 15:19:43 +0900

    add cmdline

--- src/include/cmdline/option.h
+++ src/include/cmdline/option.h
@@ -17,11 +17,12 @@ namespace utils {
 
         enum class OptionAttribute {
             none = 0,
-            required = 0x1,     // option must set
-            must_assign = 0x2,  // value must set with `=`
+            required = 0x1,        // option must set
+            must_assign = 0x2,     // value must set with `=`
+            no_option_like = 0x4,  // some value disallow option like string
         };
 
-        template <class String, template <class...> class Vec>
+        template <class String>
         struct Option {
            protected:
             OptionType type_;
@@ -37,41 +38,39 @@ namespace utils {
             }
         };
 
-        template <class String, template <class...> class Vec>
-        struct BoolOption : Option<String, Vec> {
+        template <class String>
+        struct BoolOption : Option<String> {
             BoolOption()
-                : Option<String, Vec>(OptionType::boolean) {}
+                : Option<String>(OptionType::boolean) {}
             bool flag = false;
         };
 
-        template <class String, template <class...> class Vec>
-        struct IntOption : Option<String, Vec> {
+        template <class String>
+        struct IntOption : Option<String> {
             IntOption()
-                : Option<String, Vec>(OptionType::integer) {}
+                : Option<String>(OptionType::integer) {}
             std::int64_t value = 0;
         };
 
-        template <class String, template <class...> class Vec>
-        struct StringOption : Option<String, Vec> {
+        template <class String>
+        struct StringOption : Option<String> {
             StringOption()
                 : Option<String, Vec>(OptionType::string) {}
             String value;
         };
 
-        template <class String, template <class...> class Vec>
-        struct SomeValueIntOption : Option<String, Vec> {
+        template <class String>
+        struct SomeValueIntOption : Option<String> {
             SomeValueIntOption()
-                : Option<String, Vec>(OptionType::somevalue_int) {}
-            Vec<std::int64_t> values;
+                : Option<String>(OptionType::somevalue_int) {}
             size_t minimum = 0;
             size_t maximum = 0;
         };
 
-        template <class String, template <class...> class Vec>
-        struct SomeValueStringOption : Option<String, Vec> {
+        template <class String>
+        struct SomeValueStringOption : Option<String> {
             SomeValueStringOption()
-                : Option<String, Vec>(OptionType::somevalue_string) {}
-            Vec<String> values;
+                : Option<String>(OptionType::somevalue_string) {}
             size_t minimum = 0;
             size_t maximum = 0;
         };
--- src/include/cmdline/option_result.h
+++ src/include/cmdline/option_result.h
@@ -5,8 +5,14 @@
 
 #include "option.h"
 
+#include "../wrap/lite/smart_ptr.h"
+
 namespace utils {
     namespace cmdline {
 
-    }
+        template <class String, template <class...> class Vec>
+        struct OptionResult {
+            wrap::shared_ptr<Option<String>> base;
+        };
+    }  // namespace cmdline
 }  // namespace utils
