6b5bdbf0ee6da60e92c62ba4a4c158ef803c3857
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-14 22:50:14 +0900

    add cmdline

--- src/test/number/test_number.cpp
+++ src/test/number/test_number.cpp
@@ -4,13 +4,11 @@
 
 constexpr bool test_is_number() {
     bool is_float;
-    return utils::number::is_number("fef", 16, &is_float);
+    return utils::number::is_number("fef.p1", 16, &is_float);
 }
 
 void test_number() {
     constexpr bool result1 = test_is_number();
-    0xfef.;
-    0.;
 }
 
 int main() {
