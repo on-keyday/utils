c9ce40f9689c9da316ae5e409093d57a425dc2b6
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-17 12:24:44 +0900

    refactor cmdline

--- src/include/cmdline/parse.h
+++ src/include/cmdline/parse.h
@@ -125,6 +125,17 @@ namespace utils {
                     return false;
                 });
             }
+
+            template <template <class...> class Vec, class String, class Char>
+            ParseError parse_string(int& index, int argc, Char** argv,
+                                    wrap::shared_ptr<Option<String>>& opt,
+                                    ParseFlag flag, String* assign, std::int64_t* b,
+                                    OptValue<>* target) {
+                return parse_value(index, argc, argv, opt, flag, assign, b, target, [](auto& result, auto& value) {
+                    result = value;
+                    return true;
+                });
+            }
         }  // namespace internal
 
         template <class String, class Char, template <class...> class Map, template <class...> class Vec>
