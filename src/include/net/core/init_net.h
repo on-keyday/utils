20a6b6a1059314861fcbe56cc3dbe911c0dab10f
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-21 01:03:52 +0900

    add net

---
+++ src/include/net/core/init_net.h
@@ -0,0 +1,27 @@
+/*
+    utils - utility library
+    Copyright (c) 2021 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+// init_net - initialize network
+#pragma once
+
+namespace utils {
+    namespace net {
+
+        struct Init {
+           private:
+            Init();
+            ~Init();
+            friend Init& network();
+
+           public:
+            bool initialized();
+        };
+
+        Init& network();
+    }  // namespace net
+}  // namespace utils
---
+++ src/lib/net/core/init_net.cpp
@@ -0,0 +1,51 @@
+/*
+    utils - utility library
+    Copyright (c) 2021 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+#include "../../../include/net/core/init_net.h"
+#include "../../../include/net/core/platform.h"
+
+namespace utils {
+    namespace net {
+
+        namespace internal {
+            struct InitImpl {
+#ifdef _WIN32
+                ::WSAData data;
+                bool succeed = false;
+                InitImpl() {
+                    succeed = ::WSAStartup(MAKEWORD(2, 2), &data) == 0;
+                }
+
+                ~InitImpl() {
+                    ::WSACleanup();
+                }
+#else
+                bool succeed = true;
+#endif
+            };
+            InitImpl& init() {
+                static InitImpl net;
+            }
+        }  // namespace internal
+
+        Init& network() {
+            static Init init;
+            return init;
+        }
+
+        Init& placeholder = network();
+
+        Init::Init() {
+            internal::init();
+        }
+
+        bool initialized() {
+            return internal::init().succeed;
+        }
+    }  // namespace net
+}  // namespace utils
