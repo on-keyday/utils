7b633a7edd02a7cc1e63b3ee1a7d7a59857ee2ab
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-20 15:07:48 +0900

    add net

--- src/include/helper/deref.h
+++ src/include/helper/deref.h
@@ -9,8 +9,23 @@
 // deref - dereference wrapper
 #pragma once
 
+#include "sfinae.h"
+#include <memory>
+
 namespace utils {
     namespace helper {
-
-    }
+        namespace internal {
+            SFINAE_BLOCK_T_BEGIN(derefable, *std::declval<T>())
+            using type = decltype(std::addressof(*std::declval<T&>()));
+            static auto deref(T& t) {
+                return std::addressof(*t);
+            }
+            SFINAE_BLOCK_T_ELSE(derefable)
+            using type = decltype(std::addressof(std::declval<T&>()));
+            static auto deref(T& t) {
+                return std::addressof(t);
+            }
+            SFINAE_BLOCK_T_END()
+        }  // namespace internal
+    }      // namespace helper
 }  // namespace utils
