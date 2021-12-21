72b635ceb730d53cfe8111b3d6ef770bd695e0ef
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-21 14:23:45 +0900

    add net

--- src/include/net/util/base64.h
+++ src/include/net/util/base64.h
@@ -14,8 +14,28 @@
 namespace utils {
     namespace net {
         namespace base64 {
+
+            constexpr std::uint8_t get_char(std::uint8_t num, std::uint8_t c62 = '+', std::uint8_t c63 = '/') {
+                if (num < 26) {
+                    return 'A' + num;
+                }
+                else if (num >= 26 && num <= 52) {
+                    return 'a' + (num - 26);
+                }
+                else if (num >= 52 && num < 62) {
+                    return '0' + (num - 52);
+                }
+                else if (num == 62) {
+                    return c62;
+                }
+                else if (num == 63) {
+                    return c63;
+                }
+                return 0;
+            }
+
             template <class T, class Out>
-            bool convert(Sequencer<T>& seq, Out& out) {
+            bool encode(Sequencer<T>& seq, Out& out) {
             }
         }  // namespace base64
     }      // namespace net
