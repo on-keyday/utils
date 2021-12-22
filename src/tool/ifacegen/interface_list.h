b6ca4ebe6251dc75d0ca87c2a6b89f163e147b55
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-22 21:37:04 +0900

    add net

---
+++ src/tool/ifacegen/interface_list.h
@@ -0,0 +1,14 @@
+/*
+    utils - utility library
+    Copyright (c) 2021 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+#include "../../include/wrap/lite/lite.h"
+
+namespace ifacegen {
+    namespace utw = utils::wrap;
+
+}
