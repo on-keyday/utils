8cfd54eca73864795f7676d19d9903ad840bafd7
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-22 14:22:11 +0900

    add net

--- src/test/net/test_base64.cpp
+++ src/test/net/test_base64.cpp
@@ -8,21 +8,21 @@
 
 #include "../../include/net/util/base64.h"
 
-auto test_encode() {
-    utils::helper::FixedPushBacker<char[64], 64> buf;
-    utils::net::base64::encode("hello", buf);
+auto test_encode(auto& in) {
+    utils::helper::FixedPushBacker<char[120], 119> buf;
+    utils::net::base64::encode(in, buf);
     return buf;
 }
 
 auto test_decode(auto& in) {
-    utils::helper::FixedPushBacker<char[5], 5> buf;
+    utils::helper::FixedPushBacker<char[120], 119> buf;
     utils::net::base64::decode(in, buf);
     return buf;
 }
 
 void test_base64() {
     using namespace utils::net;
-    auto result = test_encode();
+    auto result = test_encode("hello");
     auto result2 = test_decode(result.buf);
 }
 
