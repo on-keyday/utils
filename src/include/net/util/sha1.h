fe16f1d98621a74343656ae05a689a01686954ad
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-22 17:22:19 +0900

    add net

---
+++ src/include/net/util/sha1.h
@@ -0,0 +1,77 @@
+/*
+    utils - utility library
+    Copyright (c) 2021 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+// sha1 - sha1 hash
+// almost copied from commonlib
+// use on websocket
+#pragma once
+
+#include "../../endian/reader.h"
+#include "../../helper/view.h"
+
+namespace utils {
+    namespace net {
+        namespace sha1 {
+            namespace internal {
+                void calc_hash(std::uint32_t* h, const char* bits) {
+                    auto rol = [](unsigned int word, auto shift) {
+                        return (word << shift) | (word >> (sizeof(word) * 8 - shift));
+                    };
+                    std::uint32_t w[80] = {0};
+                    std::uint32_t a = h[0], b = h[1], c = h[2], d = h[3], e = h[4];
+                    for (auto i = 0; i < 80; i++) {
+                        if (i < 16) {
+                            w[i] = endian::to_network_us<std::uint32_t>(&bits[i * 4]);
+                        }
+                        else {
+                            w[i] = rol(w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1);
+                        }
+                        std::uint32_t f = 0, k = 0;
+                        if (i <= 19) {
+                            f = (b & c) | ((~b) & d);
+                            k = 0x5A827999;
+                        }
+                        else if (i <= 39) {
+                            f = b ^ c ^ d;
+                            k = 0x6ED9EBA1;
+                        }
+                        else if (i <= 59) {
+                            f = (b & c) | (b & d) | (c & d);
+                            k = 0x8F1BBCDC;
+                        }
+                        else {
+                            f = b ^ c ^ d;
+                            k = 0xCA62C1D6;
+                        }
+                        unsigned int tmp = rol(a, 5) + f + e + k + w[i];
+                        e = d;
+                        d = c;
+                        c = rol(b, 30);
+                        b = a;
+                        a = tmp;
+                    }
+                    h[0] += a;
+                    h[1] += b;
+                    h[2] += c;
+                    h[3] += d;
+                    h[4] += e;
+                }
+            }  // namespace internal
+
+            template <class T>
+            bool make_hash(Sequencer<T>& t) {
+                unsigned int hash[] = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0};
+                endian::Reader<std::remove_reference_t<T>&> r{t.buf};
+                helper::FixedPushBacker<std::uint8_t[64], 0> b;
+                auto red = r.read_seq<std::uint8_t, decltype(b), 1, 0, true>(b, 64, 0);
+                std::uint64_t* ptr = reinterpret_cast<std::uint64_t*>(b.buf);
+            }
+        }  // namespace sha1
+
+    }  // namespace net
+}  // namespace utils
