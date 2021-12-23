ebc400ea2fb50b74bae461062ab309d00a658f6e
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-23 16:14:58 +0900

    add iface gen

--- src/tool/ifacegen/interface_gen.cpp
+++ src/tool/ifacegen/interface_gen.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv) {
         .set("help,h", bool_option(true), "show help", OptFlag::none)
         .set("header,H", str_option(""), "additional header file", OptFlag::need_value, "filename")
         .set("expand,e", bool_option(true), "expand alias", OptFlag::once_in_cmd)
-        .set("no-vtable,V", bool_option(true), "add no vtable (for windows)", OptFlag::once_in_cmd)
+        .set("no-vtable,V", bool_option(true), "add __declspec(novtable) (for windows)", OptFlag::once_in_cmd)
         .set("no-rtti", multi_option<wrap::string>(2), "use type and func instead of `const std::type_info&` and `typeid(T)`", OptFlag::once_in_cmd, "type func");
     int index = 1;
     DefaultSet result;
