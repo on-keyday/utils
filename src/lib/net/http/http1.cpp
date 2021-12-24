7861e3ec3a6a64725259639b9baa45bf1ec4cbb9
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-24 20:42:39 +0900

    fix generate

--- src/lib/net/http/http1.cpp
+++ src/lib/net/http/http1.cpp
@@ -7,9 +7,38 @@
 
 
 #include "../../../include/net/http/http1.h"
+#include "../../../include/wrap/lite/string.h"
+#include "../../../include/wrap/lite/map.h"
+#include "../../../include/wrap/lite/vector.h"
+
+#include "../../../include/helper/strutil.h"
+
+#include <algorithm>
 
 namespace utils {
     namespace net {
+        namespace internal {
+            struct HeaderImpl {
+                wrap::vector<std::pair<wrap::string, wrap::string>> order;
 
-    }
+                wrap::string* find(auto& key, size_t idx = 0) {
+                    size_t count = 0;
+                    auto found = std::find_if(order.begin(), order.end(), [&](auto& v) {
+                        if (helper::equal(std::get<0>(v), key, helper::ignore_case())) {
+                            if (idx == count) {
+                                return true;
+                            }
+                            count++;
+                        }
+                        return false;
+                    });
+                    if (found != order.end()) {
+                        return std::addressof(std::get<0>(*found));
+                    }
+                    return nullptr;
+                }
+            };
+        }  // namespace internal
+
+    }  // namespace net
 }  // namespace utils
