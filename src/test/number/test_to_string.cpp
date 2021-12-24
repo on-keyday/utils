260b6f2c9136761e0a9f8416b96339ab08f1b8d3
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-25 01:43:14 +0900

    add to string

--- src/test/number/test_to_string.cpp
+++ src/test/number/test_to_string.cpp
@@ -22,7 +22,7 @@ void test_to_string() {
     constexpr auto result = test_to_string_num(10);
     static_assert(utils::helper::equal(result.buf, "10"), "expect 10 but assetion failed");
     constexpr auto result2 = test_to_string_num(-12394);
-    static_assert(utils::helper::equal(result2.buf, "-12394"), "expect 12394 but assetion failed");
+    static_assert(utils::helper::equal(result2.buf, "-12394"), "expect -12394 but assetion failed");
     constexpr auto result3 = test_to_string_num(0xff, 16);
     static_assert(utils::helper::equal(result3.buf, "ff"), "expect ff but assetion failed");
 }
