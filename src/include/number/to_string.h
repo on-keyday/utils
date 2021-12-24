057316b33f3c3659f471967824630da5b3052831
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-25 00:07:00 +0900

    add header parse

---
+++ src/include/number/to_string.h
@@ -0,0 +1,21 @@
+/*
+    utils - utility library
+    Copyright (c) 2021 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+// to_string - number to string
+#pragma once
+
+#include <limits>
+
+namespace utils {
+    namespace number {
+        template <class T>
+        T radix_mod_zero() {
+        }
+
+    }  // namespace number
+}  // namespace utils
