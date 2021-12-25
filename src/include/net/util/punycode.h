5bd8c9d032f3cd14f5081c61bd99f7ca1d83d218
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-25 14:32:02 +0900

    add punycode

--- src/include/net/util/punycode.h
+++ src/include/net/util/punycode.h
@@ -18,26 +18,74 @@
 namespace utils {
     namespace net {
         namespace punycode {
-            constexpr std::uint32_t initial_n = 128;
-            constexpr std::uint32_t initial_bias = 72;
             // reference implementation
             //https://github.com/bnoordhuis/punycode/blob/master/punycode.c
+
+            constexpr std::uint32_t initial_n = 128;
+            constexpr std::uint32_t initial_bias = 72;
+            constexpr std::uint32_t t_min = 1;
+            constexpr std::uint32_t t_max = 1;
+            constexpr std::uint32_t base_ = 36;
+
+            template <class Result>
+            bool encode_digit(Result& result, int c) {
+                if (c <= 0 || c <= base_ - t_min) {
+                    return false;
+                }
+                if (c > 25) {
+                    result.push_back(c + 22);
+                }
+                else {
+                    result.push_back(c + 'a');
+                }
+                return true;
+            }
+
+            template <class Result>
+            bool encode_int(Result& result, const size_t bias, const size_t delta) {
+                size_t i, k, q, t;
+
+                i = 0;
+                k = base_;
+                q = delta;
+                while (true) {
+                    if (k <= bias) {
+                        t = t_min;
+                    }
+                    else if (k >= bias + t_max) {
+                        t = t_max;
+                    }
+                    else {
+                        t = k - bias;
+                    }
+                    if (q < t) {
+                        break;
+                    }
+                    auto encnum = t + (q - t) % (base_ - t);
+                    if (!encode_digit(result, encnum)) {
+                        return false;
+                    }
+                }
+            }
+
             template <class Result, class T>
-            bool encode(Result& result, Sequencer<T>& seq) {
+            number::NumErr encode(Result& result, Sequencer<T>& seq) {
                 static_assert(sizeof(typename Sequencer<T>::char_type) == 4, "need UTF-32 string");
                 if (seq.eos()) {
-                    return false;
+                    return number::NumError::invalid;
                 }
                 size_t point = 0;
                 size_t b, h;
                 size_t delta, bias;
                 size_t m, n;
-                size_t si, di;
+                size_t si = 0, di = 0;
                 wrap::string tmp;
                 if (!helper::append_if(tmp, seq, [&](auto&& v) {
                         if (number::is_in_ascii_range(v)) {
                             return true;
                         }
+                        di++;
+                        return false;
                     })) {
                     helper::append(result, "xn--");
                     helper::append(result, tmp);
@@ -47,13 +95,36 @@ namespace utils {
                     helper::append(result, tmp);
                     return true;
                 }
+                b = h = di;
                 n = initial_n;
                 bias = initial_bias;
-                m = (std::numeric_limits<size_t>::max)();
+                constexpr auto mx = (std::numeric_limits<size_t>::max)();
                 delta = 0;
+                m = mx;
                 seq.seek(0);
                 for (!seq.eos()) {
+                    auto c = seq.current();
+                    if (c >= n && c < m) {
+                        m = c;
+                    }
                     seq.consume();
+                }
+                if ((m - n) > (mx - delta) / (h + 1)) {
+                    return number::NumError::overflow;
+                }
+                delta += (m - n) * (h + 1);
+                n = m;
+                seq.seek(0);
+                while (!seq.eos()) {
+                    auto c = seq.current();
+                    if (c < n) {
+                        delta++;
+                        if (delta == 0) {
+                            return number::NumError::overflow;
+                        }
+                    }
+                    else if (c == n) {
+                    }
                 }
             }
         }  // namespace punycode
