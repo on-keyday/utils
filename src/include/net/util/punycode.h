1e90b96c647bf2e9371b2c9c5e137443c1fedb6a
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-25 13:32:09 +0900

    add punycode

--- src/include/net/util/punycode.h
+++ src/include/net/util/punycode.h
@@ -9,13 +9,51 @@
 // punycode - punycode encoding
 #pragma once
 #include "../../helper/readutil.h"
+#include "../../helper/pushbacker.h"
+#include "../../helper/appender.h"
+#include "../../number/char_range.h"
+#include "../../wrap/lite/string.h"
+#include <limits>
 
 namespace utils {
     namespace net {
         namespace punycode {
-            template <class Result>
-            bool encode(Result& result) {
-                helper::append_if();
+            constexpr std::uint32_t initial_n = 128;
+            constexpr std::uint32_t initial_bias = 72;
+            // reference implementation
+            //https://github.com/bnoordhuis/punycode/blob/master/punycode.c
+            template <class Result, class T>
+            bool encode(Result& result, Sequencer<T>& seq) {
+                static_assert(sizeof(typename Sequencer<T>::char_type) == 4, "need UTF-32 string");
+                if (seq.eos()) {
+                    return false;
+                }
+                size_t point = 0;
+                size_t b, h;
+                size_t delta, bias;
+                size_t m, n;
+                size_t si, di;
+                wrap::string tmp;
+                if (!helper::append_if(tmp, seq, [&](auto&& v) {
+                        if (number::is_in_ascii_range(v)) {
+                            return true;
+                        }
+                    })) {
+                    helper::append(result, "xn--");
+                    helper::append(result, tmp);
+                    result.push_back('-');
+                }
+                else {
+                    helper::append(result, tmp);
+                    return true;
+                }
+                n = initial_n;
+                bias = initial_bias;
+                delta = 0;
+                seq.seek(0);
+                for (!seq.eos()) {
+                    seq.consume();
+                }
             }
         }  // namespace punycode
 
