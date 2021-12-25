ac1cb355584f629b9d68c3bac74aebf964596e22
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-25 23:35:16 +0900

    add punycode

--- src/test/net/test_punycode.cpp
+++ src/test/net/test_punycode.cpp
@@ -11,9 +11,10 @@
 #include "../../include/helper/strutil.h"
 
 void test_punycode() {
-    utils::helper::FixedPushBacker<char[100], 99> buf;
+    utils::helper::FixedPushBacker<char[50], 49> buf, buf2;
     utils::net::punycode::encode(u8"ウィキペディア", buf);
     assert(utils::helper::equal(buf.buf, "cckbak0byl6e"));
+    utils::net::punycode::decode(buf.buf, buf2);
 }
 
 int main() {
