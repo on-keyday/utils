6219e976c547fbe8495242f05213b7d4b6b979dd
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-25 15:15:04 +0900

    add punycode

--- src/include/net/util/punycode.h
+++ src/include/net/util/punycode.h
@@ -156,6 +156,7 @@ namespace utils {
                         seq.consume();
                     }
                 }
+                return true;
             }
         }  // namespace punycode
 
