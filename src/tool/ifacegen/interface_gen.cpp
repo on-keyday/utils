a71b5d8d1f8f1bdc959755ae8dbd9401d76cae9c
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-26 15:29:52 +0900

    add punycode

--- src/tool/ifacegen/interface_gen.cpp
+++ src/tool/ifacegen/interface_gen.cpp
@@ -69,8 +69,8 @@ int main(int argc, char** argv) {
 
 Special Value:
     Func Name:
-        decltype - generate type assert func ex) decltype() type # type is placeholder
-        __copy__ - generate copy constructor and copy assign ex) __copy__() type # type is placeholder
+        decltype - generate type assert func ex) decltype() type (`type` is placeholder)
+        __copy__ - generate copy constructor and copy assign ex) __copy__() type (`type` is placeholder)
         __call__ - generate `operator()` ex) __call__(num int) int 
     Default Value:
         panic - generate throw std::bad_function_call()
