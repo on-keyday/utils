10630926f7cc6a551a8c6e77a356498ed163cd10
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-26 18:59:13 +0900

    add http1

--- src/test/net/test_http1.cpp
+++ src/test/net/test_http1.cpp
@@ -6,7 +6,44 @@
 */
 
 
+#include "../../include/net/tcp/tcp.h"
+#include "../../include/net/ssl/ssl.h"
+#include "../../include/wrap/lite/string.h"
+#include "../../include/wrap/cout.h"
 #include "../../include/net/http/http1.h"
+
+#include <windows.h>
+
+utils::net::IOClose make_ioclose() {
+    auto query = utils::net::query_dns("google.com", "https");
+    auto addr = query.get_address();
+    while (!addr) {
+        if (query.failed()) {
+            assert(false && "dns query failed");
+        }
+        Sleep(10);
+        addr = query.get_address();
+    }
+    auto result = utils::net::open(std::move(addr));
+    auto sock = result.connect();
+    while (!sock) {
+        if (result.failed()) {
+            assert(false && "connect socket failed");
+        }
+        Sleep(10);
+        sock = result.connect();
+    }
+    auto sslres = utils::net::open(std::move(sock), "./src/test/net/cacert.pem");
+    auto ssl = sslres.connect();
+    while (!ssl) {
+        if (sslres.failed()) {
+            assert(false && "connect ssl failed");
+        }
+        Sleep(10);
+        ssl = sslres.connect();
+    }
+    return ssl;
+}
 
 void test_http1() {
     utils::net::request();
