ae9164d2f363edc41f47e9c276dda6624b3a3003
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-26 17:23:12 +0900

    add http1

--- src/include/net/http/body.h
+++ src/include/net/http/body.h
@@ -12,6 +12,7 @@
 #include "../../core/sequencer.h"
 #include "../core/iodef.h"
 #include "../../helper/readutil.h"
+#include "../../number/parse.h"
 
 namespace utils {
     namespace net {
@@ -23,11 +24,28 @@ namespace utils {
             };
 
             template <class String, class T>
-            bool read_body(String& result, Sequencer<T>& seq, BodyType type = BodyType::no_info, size_t expect = 0) {
+            State read_body(String& result, Sequencer<T>& seq, BodyType type = BodyType::no_info, size_t expect = 0) {
                 auto inipos = seq.rptr;
-                if (type == BodyType::no_info) {
+                if (type == BodyType::chuncked) {
+                    size_t num = 0;
+                    auto e = number::parse_integer(seq, num, 16);
+                    if (e != number::NumError::none) {
+                        return State::failed;
+                    }
+                    if (!helper::match_eol(seq)) {
+                        return State::failed;
+                    }
+                }
+                else if (type == BodyType::content_length) {
+                    if (seq.remain() < expect) {
+                        return State::running;
+                    }
+                    helper::read_n(result, seq, expect);
+                    return State::complete;
+                }
+                else {
                     helper::read_all(result, seq);
-                    return true;
+                    return State::complete;
                 }
             }
         }  // namespace h1body
