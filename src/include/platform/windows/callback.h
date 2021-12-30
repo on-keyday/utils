3bb967eeae23a5d24add3bbc44f0c00790c923c1
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-30 21:34:48 +0900

    regenerate

--- src/include/platform/windows/callback.h
+++ src/include/platform/windows/callback.h
@@ -10,70 +10,74 @@
 #pragma once
 #include "../../helper/deref.h"
 
-namespace utils::platform::windows {
-    struct Complete {
-       private:
-        struct interface__ {
-            virtual void operator()(size_t size) = 0;
+namespace utils {
+    namespace platform {
+        namespace windows {
+            struct Complete {
+               private:
+                struct interface__ {
+                    virtual void operator()(size_t size) = 0;
 
-            virtual ~interface__() {}
-        };
+                    virtual ~interface__() {}
+                };
 
-        template <class T__>
-        struct implements__ : interface__ {
-            T__ t_holder_;
+                template <class T__>
+                struct implements__ : interface__ {
+                    T__ t_holder_;
 
-            template <class V__>
-            implements__(V__&& args)
-                : t_holder_(std::forward<V__>(args)) {}
+                    template <class V__>
+                    implements__(V__&& args)
+                        : t_holder_(std::forward<V__>(args)) {}
 
-            void operator()(size_t size) override {
-                auto t_ptr_ = utils::helper::deref(this->t_holder_);
-                if (!t_ptr_) {
-                    return (void)0;
+                    void operator()(size_t size) override {
+                        auto t_ptr_ = utils::helper::deref(this->t_holder_);
+                        if (!t_ptr_) {
+                            return (void)0;
+                        }
+                        return (*t_ptr_)(size);
+                    }
+                };
+
+                interface__* iface = nullptr;
+
+               public:
+                constexpr Complete() {}
+
+                constexpr Complete(std::nullptr_t) {}
+
+                template <class T__>
+                Complete(T__&& t) {
+                    if (!utils::helper::deref(t)) {
+                        return;
+                    }
+                    iface = new implements__<std::decay_t<T__>>(std::forward<T__>(t));
                 }
-                return (*t_ptr_)(size);
-            }
-        };
 
-        interface__* iface = nullptr;
+                Complete(Complete&& in) {
+                    iface = in.iface;
+                    in.iface = nullptr;
+                }
 
-       public:
-        constexpr Complete() {}
+                Complete& operator=(Complete&& in) {
+                    delete iface;
+                    iface = in.iface;
+                    in.iface = nullptr;
+                    return *this;
+                }
 
-        constexpr Complete(std::nullptr_t) {}
+                explicit operator bool() const {
+                    return iface != nullptr;
+                }
 
-        template <class T__>
-        Complete(T__&& t) {
-            if (!utils::helper::deref(t)) {
-                return;
-            }
-            iface = new implements__<std::decay_t<T__>>(std::forward<T__>(t));
-        }
+                ~Complete() {
+                    delete iface;
+                }
 
-        Complete(Complete&& in) {
-            iface = in.iface;
-            in.iface = nullptr;
-        }
+                void operator()(size_t size) {
+                    return iface ? iface->operator()(size) : (void)0;
+                }
+            };
 
-        Complete& operator=(Complete&& in) {
-            delete iface;
-            iface = in.iface;
-            in.iface = nullptr;
-            return *this;
-        }
-
-        explicit operator bool() const {
-            return iface != nullptr;
-        }
-
-        ~Complete() {
-            delete iface;
-        }
-
-        void operator()(size_t size) {
-            return iface ? iface->operator()(size) : (void)0;
-        }
-    };
-
-}  // namespace utils::platform::windows
+        }  // namespace windows
+    }      // namespace platform
+}  // namespace utils
