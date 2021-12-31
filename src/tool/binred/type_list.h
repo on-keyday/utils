e8540d59ac65a773f0bfc15d4ab867aedc6d8acb
K'sPC <62627905+on-keyday@users.noreply.github.com>
2021-12-31 22:22:14 +0900

    add tool

--- src/tool/binred/type_list.h
+++ src/tool/binred/type_list.h
@@ -2,16 +2,40 @@
 // binred - binary reader helper
 #pragma once
 
-#include "../../include/wrap/lite/string.h"
+#include "../../include/wrap/lite/lite.h"
 
 namespace binred {
     namespace utw = utils::wrap;
+    enum class FlagType {
+        none,
+        eq,
+        bit,
+    };
+
     struct Flag {
         utw::string depend;
-        size_t num;
+        size_t num = 0;
+        FlagType type = FlagType::none;
     };
 
     struct Type {
         utw::string name;
+        Flag flag;
     };
+
+    struct Member {
+        utw::string name;
+        Type type;
+        utw::string defval;
+    };
+
+    struct Struct {
+        utw::vector<Member> member;
+    };
+
+    struct FileData {
+        utw::map<utw::string, Struct> structs;
+        utw::vector<utw::string> defvec;
+    };
+
 }  // namespace binred
