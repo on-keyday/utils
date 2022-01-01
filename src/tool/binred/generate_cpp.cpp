71206a6f6ca951eed8a421ad280477be12cac164
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-01 16:38:37 +0900

    add tool

--- src/tool/binred/generate_cpp.cpp
+++ src/tool/binred/generate_cpp.cpp
@@ -23,7 +23,24 @@ namespace binred {
                 }
                 hlp::append(str, ";\n");
             }
-            hlp::append(str, "};\n");
+            hlp::append(str, "};\n\n");
+            hlp::appends(str, "template<class Output>\nbool encode(", d.first, "& input,OutPut& output){\n");
+            for (auto& memb : d.second.member) {
+                auto& flag = memb.type.flag;
+                auto has_flag = flag.type != FlagType::none;
+                if (has_flag) {
+                    if (flag.type == FlagType::eq) {
+                        hlp::appends(str, "    if (", flag.depend, " == ", flag.val, ") {\n    ");
+                    }
+                    else if (flag.type == FlagType::bit) {
+                        hlp::appends(str, "    if (", flag.depend, "&", flag.val, ") {\n    ");
+                    }
+                }
+                hlp::appends(str, "    output.write(", memb.name, ");\n");
+                if (has_flag) {
+                    hlp::append(str, "    }\n");
+                }
+            }
         }
     }
 }  // namespace binred
