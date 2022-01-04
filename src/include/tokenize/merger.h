4e760b2958c44dfad4b1abcee40e3b63558ae27f
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-04 22:52:13 +0900

    add parse dispatcher

---
+++ src/include/tokenize/merger.h
@@ -0,0 +1,37 @@
+/*
+    utils - utility library
+    Copyright (c) 2021 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+// merger - define merge contexts
+#pragma once
+#include "merge.h"
+
+namespace utils {
+    namespace tokenize {
+
+        template <class String>
+        internal::CommentMergeContext<String> c_comment(bool nest = false) {
+            return blcok_comment<String>("/*", "*/", nest);
+        }
+
+        template <class String>
+        internal::CommentMergeContext<String> string(bool allow_line = false) {
+            return escaped_comment<String>("\"", "\\", allow_line);
+        }
+
+        template <class String>
+        internal::CommentMergeContext<String> cpp_comment() {
+            return line_comment<String>("//");
+        }
+
+        template <class String>
+        internal::CommentMergeContext<String> sh_comment() {
+            return line_comment<String>("#");
+        }
+
+    }  // namespace tokenize
+}  // namespace utils
