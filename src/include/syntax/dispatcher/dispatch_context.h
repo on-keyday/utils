c345e4732e34be4a83eeaeb7bcebd0fb2046908e
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-04 21:58:13 +0900

    add parse dispatcher

--- src/include/syntax/dispatcher/dispatch_context.h
+++ src/include/syntax/dispatcher/dispatch_context.h
@@ -39,7 +39,13 @@ namespace utils {
             Vec<DispatchFilter<context_t>> disp;
 
             template <class F, class B = decltype(helper::no_check())>
-            void append(F&& f, FilterType type, B&& filter = helper::no_check()) {
+            void append(F&& f, FilterType type = FilterType::filter, B&& filter = helper::no_check()) {
+                disp.push_back(
+                    DispatchFilter<context_t>{
+                        .dispatch = std::forward<F>(f),
+                        .type = type,
+                        .filter = std::forward<B>(filter),
+                    });
             }
 
             MatchState operator()(context_t& ctx) {
