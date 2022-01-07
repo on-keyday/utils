35951d25b4bb9da6b0b86884ed70f2916013a26d
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-07 14:58:31 +0900

    add json

---
+++ src/include/net/util/json/json.h
@@ -0,0 +1,16 @@
+/*
+    utils - utility library
+    Copyright (c) 2021-2022 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+// json - json object
+#pragma once
+
+namespace utils {
+    namespace net {
+
+    }
+}  // namespace utils
