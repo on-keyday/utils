5b8bc50bde06311aa650e011373213e8b542882f
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-08 11:18:51 +0900

    add equal

--- src/test/escape/test_escape.cpp
+++ src/test/escape/test_escape.cpp
@@ -10,18 +10,20 @@
 #include "../../include/helper/pushbacker.h"
 #include "../../include/helper/equal.h"
 
-constexpr auto test_escape_str() {
+constexpr auto test_escape_str(utils::escape::EscapeFlag flag) {
     namespace ue = utils::escape;
     namespace uh = utils::helper;
     auto seq = utils::make_ref_seq(u8"\n\t\rあ");
     uh::FixedPushBacker<char[30], 29> out;
-    ue::escape_str(seq, out, ue::EscapeFlag::utf);
+    ue::escape_str(seq, out, flag);
     return out;
 }
 
 void test_escape() {
-    constexpr auto e = test_escape_str();
+    constexpr auto e = test_escape_str(utils::escape::EscapeFlag::utf);
     static_assert(utils::helper::equal("\\n\\t\\r\\u3042", e.buf), "expect true but assertion failed");
+    constexpr auto o = test_escape_str(utils::escape::EscapeFlag::hex);
+    static_assert(utils::helper::equal("\\n\\t\\r\\xe3\\x81\\x82", o.buf), "expect true but assertion failed");
 }
 
 int main() {
