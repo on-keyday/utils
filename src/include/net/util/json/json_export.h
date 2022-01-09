9730cbc6da2cdc2567b022374c5f7dced39c1dba
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-09 16:37:26 +0900

    add json

---
+++ src/include/net/util/json/json_export.h
@@ -0,0 +1,27 @@
+/*
+    utils - utility library
+    Copyright (c) 2021-2022 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+// json_export - export json
+#pragma once
+
+#include "json.h"
+
+namespace utils {
+    namespace net {
+        namespace json {
+#if !defined(UTILS_NET_JSON_NO_EXTERN_TEMPLATE)
+            extern
+#endif
+                template struct JSONBase<wrap::string, wrap::vector, wrap::map>;
+#if !defined(UTILS_NET_JSON_NO_EXTERN_TEMPLATE)
+            extern
+#endif
+                template struct JSONBase<wrap::string, wrap::vector, ordered_map>;
+        }  // namespace json
+    }      // namespace net
+}  // namespace utils
