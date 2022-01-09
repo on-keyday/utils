a50333fb059bdc929a4213433581dd43a67aba11
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-09 12:06:41 +0900

    add json

--- src/include/net/util/json/to_string.h
+++ src/include/net/util/json/to_string.h
@@ -19,11 +19,28 @@ namespace utils {
             template <class Out, class String, template <class...> class Vec, template <class...> class Object>
             JSONErr to_string(const JSONBase<String, Vec, Object>& json, Out& out) {
                 internal::JSONHolder<String, Vec, Object>& holder = json.get_holder();
+                auto numtostr = [&](auto& j) -> JSONErr {
+                    auto e = number::to_string(out, *j);
+                    if (!e) {
+                        return JSONError::invalid_number;
+                    }
+                    return true;
+                };
                 if (auto b = holder.as_bool()) {
                     helper::append(out, *b ? "true" : "false");
                     return true;
                 }
+                else if (auto i = holder.as_numi()) {
+                    return numtostr(*i);
+                }
+                else if (auto u = holder.as_numu()) {
+                    return numtostr(*u);
+                }
+                else if (auto f = holder.as_numf()) {
+                    return numtostr(*f);
+                }
             }
-        }  // namespace json
-    }      // namespace net
+        }
+    }  // namespace json
+}  // namespace net
 }  // namespace utils
