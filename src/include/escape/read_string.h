5b8503afa8edb53715a67e7b3dd90fb0d3d8e0b3
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-14 22:39:54 +0900

    add to json (binred invalid)

--- src/include/escape/read_string.h
+++ src/include/escape/read_string.h
@@ -57,7 +57,7 @@ namespace utils {
                 if (!line && c == '\n') {
                     return true;
                 }
-                return !(is_prefix(c) && (esc ? seq.current(-1) != '\\' || seq.current(-2) == '\\' : true));
+                return !(c == s && (esc ? seq.current(-1) != '\\' || seq.current(-2) == '\\' : true));
             });
             if (seq.current() != s) {
                 return false;
@@ -65,6 +65,7 @@ namespace utils {
             if (needdpfx) {
                 key.push_back(s);
             }
+            seq.consume();
             if (esc) {
                 String tmp;
                 if (!escape::unescape_str(key, tmp)) {
