4ecd323be763719581c87259c1c0fa4c4d23afa6
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-01-31 00:26:22 +0900

    radix digits

--- src/test/number/test_digitcount.cpp
+++ src/test/number/test_digitcount.cpp
@@ -9,6 +9,7 @@
 #include <number/radix.h>
 #include <number/insert_space.h>
 #include <wrap/cout.h>
+#include <iomanip>
 
 constexpr auto digit_value() {
     utils::number::Array<63, char> value{};
@@ -26,7 +27,8 @@ void test_digitcount() {
     constexpr auto val = digit_value();
     auto ptr = reinterpret_cast<const std::uint8_t*>(utils::number::digit_bound<T>);
     for (size_t i = 0; i < size_; i++) {
-        utils::wrap::cout_wrap() << std::hex << size_t(ptr[i]) << ",";
+        if (ptr[i] == 0) continue;
+        utils::wrap::cout_wrap() << std::setw(2) << std::hex << size_t(ptr[i]) << ",";
     }
 }
 
