c489b8e8918123bb4870e6f54fea63b73276f955
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-02-13 12:14:24 +0900

    add async tcp

--- src/test/net/async/test_asynctcp.cpp
+++ src/test/net/async/test_asynctcp.cpp
@@ -17,20 +17,33 @@ void test_asynctcp() {
     auto begin = system_clock::now();
     co.wait();
     auto end = system_clock::now();
-    auto time = duration_cast<milliseconds>(end - begin);
-    utils::wrap::cout_wrap() << time << "\n";
+    auto time = [&] {
+        return duration_cast<milliseconds>(end - begin);
+    };
+    utils::wrap::cout_wrap()
+        << time() << "\n";
     auto conn = co.get();
     if (!conn) {
         utils::wrap::cout_wrap() << "failed\n";
         return;
     }
-    auto v = net::get_pool().start([=](async::Context& ctx) mutable {
+    auto& pool = net::get_pool();
+    pool.set_yield(true);
+    auto v = pool.start([=](async::Context& ctx) mutable {
         auto text = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
         while (conn->write(text, strlen(text)) == net::State::running) {
             ctx.suspend();
         }
+        wrap::string data;
+        while (net::read(data, conn) == net::State::running) {
+            ctx.suspend();
+        }
+        utils::wrap::cout_wrap() << data;
     });
+    begin = system_clock::now();
     v.wait();
+    end = system_clock::now();
+    utils::wrap::cout_wrap() << time();
 }
 
 int main() {
