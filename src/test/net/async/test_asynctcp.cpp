77a77136713862abc7b86a8d4675e54d51c1d4f3
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-02-13 12:57:31 +0900

    add async tcp

--- src/test/net/async/test_asynctcp.cpp
+++ src/test/net/async/test_asynctcp.cpp
@@ -15,40 +15,52 @@ void test_asynctcp() {
     using namespace std::chrono;
     auto& pool = net::get_pool();
     pool.set_yield(true);
-    auto co = net::async_open("google.com", "http");
-    auto begin = system_clock::now();
-    co.wait();
-    auto end = system_clock::now();
-    auto time = [&] {
-        return duration_cast<milliseconds>(end - begin);
-    };
-    utils::wrap::cout_wrap()
-        << time() << "\n";
-    auto conn = co.get();
-    if (!conn) {
-        utils::wrap::cout_wrap() << "failed\n";
-        return;
-    }
-    auto v = pool.start([=](async::Context& ctx) mutable {
-        auto text = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
-        auto len = strlen(text);
-        auto st = conn->write(text, len);
-        while (st == net::State::running) {
-            ctx.suspend();
-            st = conn->write(text, len);
-        }
-        wrap::string data = "data:\n";
-        st = net::read(data, *conn);
-        while (st == net::State::running) {
-            ctx.suspend();
+    auto spawn = [&](const char* host) {
+        return pool.start([host](async::Context& ctx) mutable {
+            auto pack = utils::wrap::pack();
+            auto co = net::async_open(host, "http");
+            assert(co.state() != async::TaskState::invalid);
+            auto begin = system_clock::now();
+            co.wait_or_suspend(ctx);
+            auto end = system_clock::now();
+            auto time = [&] {
+                return duration_cast<milliseconds>(end - begin);
+            };
+            pack << "query:\n"
+                 << time() << "\n";
+            begin = system_clock::now();
+            auto conn = co.get();
+            assert(conn);
+            wrap::string text = "GET / HTTP/1.1\r\nHost: ";
+            text += host;
+            text += "\r\n\r\n";
+            auto st = conn->write(text.c_str(), text.size());
+            while (st == net::State::running) {
+                ctx.suspend();
+                st = conn->write(text.c_str(), text.size());
+            }
+            wrap::string data;
             st = net::read(data, *conn);
-        }
-        utils::wrap::cout_wrap() << data;
-    });
-    begin = system_clock::now();
+            while (st == net::State::running) {
+                ctx.suspend();
+                st = net::read(data, *conn);
+            }
+            end = system_clock::now();
+            pack << "data:\n"
+                 << data << "\n";
+            pack << "time:\n"
+                 << time() << "\n";
+            utils::wrap::cout_wrap() << std::move(pack);
+        });
+    };
+    auto v = spawn("google.com");
+    auto u = spawn("httpbin.org");
+    auto begin = system_clock::now();
     v.wait();
-    end = system_clock::now();
-    utils::wrap::cout_wrap() << time();
+    u.wait();
+    auto end = system_clock::now();
+    utils::wrap::cout_wrap() << "\ntotal time\n"
+                             << duration_cast<milliseconds>(end - begin) << "\n";
 }
 
 int main() {
