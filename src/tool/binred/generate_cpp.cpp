dde9ca06ec02fb427acc786d21bd9661689031cb
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-02-18 01:45:29 +0900

    binred

--- src/tool/binred/generate_cpp.cpp
+++ src/tool/binred/generate_cpp.cpp
@@ -24,7 +24,7 @@ namespace binred {
         if (tree->kw == us::KeyWord::id) {
             if (tree->token != "true" && tree->token != "false" &&
                 tree->token != "nullptr" && rel.size() && str.back() != '.' &&
-                !hlp::contains(str, "::")) {
+                !hlp::contains(tree->token, "::")) {
                 hlp::appends(str, rel, ".");
             }
         }
