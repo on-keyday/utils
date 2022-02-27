df24349308dd332fddbf628c47eabb4299914535
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-02-28 01:14:17 +0900

    new cmd parser

--- src/include/cmdline/option/optcontext.h
+++ src/include/cmdline/option/optcontext.h
@@ -69,6 +69,9 @@ namespace utils {
 
                 template <class Str>
                 bool VarStr(Str* ptr, auto&& option, auto&& help, auto&& argdesc, bool once = false) {
+                    if (!ptr) {
+                        return false;
+                    }
                     return (bool)value(
                         option, ptr,
                         StringParser<Str>{},
