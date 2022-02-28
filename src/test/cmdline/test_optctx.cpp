ac13343c4e89b2b13d251cdc4926372b7b86a10b
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-02-28 13:14:45 +0900

    new cmd parser

--- src/test/cmdline/test_optctx.cpp
+++ src/test/cmdline/test_optctx.cpp
@@ -11,9 +11,8 @@
 #include <cmdline/option/help.h>
 #include <wrap/cout.h>
 #include <wrap/argv.h>
-
-void test_optctx(int argc, char** argv) {
-    using namespace utils::cmdline;
+using namespace utils::cmdline;
+void test_optctx(int argc, char** argv, option::ParseFlag flag) {
     option::Context ctx;
     auto test = ctx.Bool("test,t", false, "test flag", "");
     auto str = ctx.String<utils::wrap::string>("str,s", "default", "help", "VALUE");
@@ -21,15 +20,14 @@ void test_optctx(int argc, char** argv) {
     ctx.VarBool(&test2, "test2,2", "test flag 2", "");
     auto vec = ctx.VecString("vector,v", 2, "vector", "STR1 STR2");
     auto veci = ctx.VecInt("int,i", 2, "int vector", "INT1 INT2");
-    auto v = option::ParseFlag::optget_ext_mode;
     auto& cout = utils::wrap::cout_wrap();
-    cout << option::desc<utils::wrap::string>(v, ctx.desc.list);
-    auto err = option::parse(argc, argv, ctx, utils::helper::nop, v);
+    cout << option::desc<utils::wrap::string>(flag, ctx.desc.list);
+    auto err = option::parse(argc, argv, ctx, utils::helper::nop, flag);
     if (auto val = error_msg(err)) {
-        cout << "error: " << ctx.result.erropt << ": " << val;
+        cout << "error: " << ctx.result.erropt << ": " << val << "\n";
     }
     else {
-        cout << "parse succeeded\n";
+        cout << "parse succeeded:\n";
         cout << "test: " << std::boolalpha << *test << "\n";
         cout << "str: " << *str << "\n";
         cout << "test2: " << std::boolalpha << test2 << "\n";
@@ -46,5 +44,6 @@ void test_optctx(int argc, char** argv) {
 
 int main(int argc, char** argv) {
     utils::wrap::U8Arg _(argc, argv);
-    test_optctx(argc, argv);
+    test_optctx(argc, argv, option::ParseFlag::golang_mode);
+    test_optctx(argc, argv, option::ParseFlag::optget_ext_mode);
 }
