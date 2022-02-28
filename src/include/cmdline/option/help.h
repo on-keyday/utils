1b8c817ab13bb3ba5f5cd5efdc2cc3411c5c69e7
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-02-28 12:45:25 +0900

    new cmd parser

--- src/include/cmdline/option/help.h
+++ src/include/cmdline/option/help.h
@@ -9,63 +9,96 @@
 // help - show option help
 #pragma once
 #include "option_set.h"
+#include "pushbacker.h"
 
 namespace utils {
     namespace cmdline {
         namespace option {
-            template <class Result, class OptVec>
-            bool desc(Result& result, ParseFlag flag, wrap::shared_ptr<Option>& opt) {
-                if (!opt) {
-                    return false;
-                }
-                auto write = [&](auto&&... v) {
-                    helper::appends(result, v...);
-                };
-                auto write_assign = [&](auto&& argdesc) {
-                    if (any(flag & ParseFlag::sf_assign)) {
-                        write(get_assignment(flag));
-                    }
-                    else {
-                        write(" ");
-                    }
-                    write(argdesc);
-                };
-                auto write_opt = [&](auto&& opt, auto&& argdesc) {
-                    bool sht = any(flag & ParseFlag::pf_short);
-                    bool log = any(flag & ParseFlag::pf_long);
-                    bool val = any(flag & ParseFlag::pf_value);
-                    if (sht && log) {
-                        if (bufsize(opt) == 1) {
-                            write("-", opt);
+            auto opts_write(auto& result) {
+                return [&](ParseFlag flag, auto& opt) {
+                    auto write = [&](auto&&... v) {
+                        helper::appends(result, v...);
+                    };
+                    auto write_assign = [&](auto&& argdesc) {
+                        if (bufsize(argdesc) == 0) {
+                            return;
+                        }
+                        if (any(flag & ParseFlag::sf_assign)) {
+                            write(get_assignment(flag));
+                        }
+                        else {
+                            write(" ");
+                        }
+                        write(argdesc);
+                    };
+                    auto write_opt = [&](auto&& opt, auto&& argdesc) {
+                        bool sht = any(flag & ParseFlag::pf_short);
+                        bool log = any(flag & ParseFlag::pf_long);
+                        bool val = any(flag & ParseFlag::pf_value);
+                        if (sht && log) {
+                            if (bufsize(opt) == 1) {
+                                write("-", opt);
+                                if (val) {
+                                    if (bufsize(argdesc)) {
+                                        write(argdesc);
+                                    }
+                                }
+                                else {
+                                    write_assign(argdesc);
+                                }
+                            }
+                            else {
+                                write("--", opt);
+                                write_assign(argdesc);
+                            }
+                        }
+                        else if (sht) {
+                            write(get_prefix(flag), opt);
                             if (val) {
-                                write(argdesc);
+                                if (bufsize(argdesc)) {
+                                    write(argdesc);
+                                }
                             }
                             else {
                                 write_assign(argdesc);
                             }
                         }
-                        else {
+                        else if (log) {
                             write("--", opt);
-                            write_assign(argdesc);
                         }
-                    }
-                    else if (sht) {
-                        write(get_prefix(flag), opt);
-                        if (val) {
-                            write(argdesc);
-                        }
-                        else {
-                            write_assign(argdesc);
-                        }
-                    }
-                    else if (log) {
-                        write("--", opt);
+                    };
+                    write_opt(opt->mainname, opt->argdesc);
+                    for (auto& alias : opt->aliases) {
+                        write(", ");
+                        write_opt(alias, opt->argdesc);
                     }
                 };
-                write_opt(opt->mainname, opt->argdesc);
-                for (auto& alias : opt->aliases) {
-                    write(", ");
-                    write_opt(alias, opt->argdesc);
+            }
+
+            template <class Result, class OptVec>
+            bool desc(Result& result, ParseFlag flag, OptVec& vec, const char* indent = "    ") {
+                helper::CountPushBacker push;
+                size_t maxlen = 0;
+                auto tmp = opts_write(push);
+                for (wrap::shared_ptr<Option>& opt : vec) {
+                    helper::append(result, indent);
+                    tmp(flag, opt);
+                    if (maxlen < push.count) {
+                        maxlen = push.count;
+                    }
+                    push.count = 0;
+                }
+                helper::CountPushBacker<Result&> cb{result};
+                auto actual = opts_write(cb);
+                for (wrap::shared_ptr<Option>& opt : vec) {
+                    helper::append(result, indent);
+                    actual(flag, opt);
+                    while (maxlen > cb.count) {
+                        cb.push_back(' ');
+                    }
+                    helper::append(result, " : ");
+                    helper::append(result, opt->help);
+                    helper::append(result, "\n");
                 }
             }
         }  // namespace option
