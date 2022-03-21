056585a446511e4cb18cea6424920284c6fcea3d
K'sPC <62627905+on-keyday@users.noreply.github.com>
2022-03-22 01:20:49 +0900

    fix heavy typo

---
+++ src/test/async/light/test_make_arg.cpp
@@ -0,0 +1,14 @@
+/*
+    utils - utility library
+    Copyright (c) 2021-2022 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+
+#include <async/light/context.h>
+using namespace utils::async::light;
+
+int test_make_arg() {
+    make_arg(9, "");
+}
