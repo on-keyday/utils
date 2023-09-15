9e8751a948b50cb2e9551ae62478ffa3b9562617
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-09-15 12:48:19 +0900

    add support for wasm build

--- build.bat
+++ build.bat
@@ -15,12 +15,18 @@ if "%UTILS_BUILD_TYPE%" == "" (
 set LLVM_DIR=D:\llvm-project\%BUILD_TYPE%
 set INSTALL_PREFIX=%CD%
 rem -D CMAKE_CXX_CLANG_TIDY=clang-tidy;-header-filter=src/include;--checks=-*
-if "%CLANG%"=="true" (
-    cmake -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_TYPE=%UTILS_BUILD_TYPE% -S . -B built/%UTILS_BUILD_TYPE%/%BUILD_TYPE%
+rem if "%CLANG%"=="true" (
+if "%UTILS_BUILD_TYPE%" == "wasm" (
+    rem emcmake must be enabled on this context
+    call emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_TYPE=static -S . -B built/%UTILS_BUILD_TYPE%/%BUILD_TYPE%
 ) else (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-    cmake -D CMAKE_CXX_COMPILER=cl -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% .
+    cmake -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_TYPE=%UTILS_BUILD_TYPE% -S . -B built/%UTILS_BUILD_TYPE%/%BUILD_TYPE%
 )
+rem ) 
+rem else (
+rem     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+rem     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+rem     cmake -D CMAKE_CXX_COMPILER=cl -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% .
+rem )
 ninja -C built/%UTILS_BUILD_TYPE%/%BUILD_TYPE% %TARGET%
 ninja -C built/%UTILS_BUILD_TYPE%/%BUILD_TYPE% install
