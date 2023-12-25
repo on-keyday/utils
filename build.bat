1d5e9ddd60f468a57c03e7966a32c0e6ac321b16
on-keyday <62627905+on-keyday@users.noreply.github.com>
2023-12-26 01:15:36 +0900

    add gcc-static and gcc-shared

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 setlocal
-set CLANG=true
 set BUILD_TYPE=%2
 set UTILS_BUILD_MODE=%1
 set TARGET=%3
@@ -24,10 +23,32 @@ if "%UTILS_BUILD_MODE%" == "wasm-rt" (
 if "%UTILS_BUILD_MODE%" == "wasm-em" (
      set TRUE_FALSE=true
 )
+if "%UTILS_BUILD_MODE%" == "gcc-shared" (
+     set TRUE_FALSE=true
+)
+if "%UTILS_BUILD_MODE%" == "gcc-static" (
+     set TRUE_FALSE=true
+)
 
 if "%TRUE_FALSE%"=="false" (
     echo "Invalid UTILS_BUILD_MODE: %UTILS_BUILD_MODE%"
     exit /b
+)
+
+set CXX_COMPILER=clang++
+set C_COMPILER=clang
+set OUTPUT_DIR=built/%UTILS_BUILD_MODE%/%BUILD_TYPE%
+
+if "%UTILS_BUILD_MODE%" == "gcc-static" (
+    set CXX_COMPILER=g++
+    set C_COMPILER=gcc
+    set UTILS_BUILD_MODE=static
+)
+
+if "%UTILS_BUILD_MODE%" == "gcc-shared" (
+    set CXX_COMPILER=g++
+    set C_COMPILER=gcc
+    set UTILS_BUILD_MODE=shared
 )
 
 
@@ -37,12 +58,12 @@ rem -D CMAKE_CXX_CLANG_TIDY=clang-tidy;-header-filter=src/include;--checks=-*
 rem if "%CLANG%"=="true" (
 if "%UTILS_BUILD_MODE%" == "wasm-rt" (
     rem WASI_SDK_PATH must be enabled on this context
-    cmake -D CMAKE_TOOLCHAIN_FILE=%WASI_SDK_PREFIX%/share/cmake/wasi-sdk-pthread.cmake -D WASI_SDK_PREFIX=%WASI_SDK_PREFIX%  -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_MODE=static -S . -B built/%UTILS_BUILD_MODE%/%BUILD_TYPE%  
+    cmake -D CMAKE_TOOLCHAIN_FILE=%WASI_SDK_PREFIX%/share/cmake/wasi-sdk-pthread.cmake -D WASI_SDK_PREFIX=%WASI_SDK_PREFIX%  -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_MODE=static -S . -B %OUTPUT_DIR%
 ) else if "%UTILS_BUILD_MODE%" == "wasm-em" (
     rem emcmake must be enabled on this context
-    call emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_MODE=static -S . -B built/%UTILS_BUILD_MODE%/%BUILD_TYPE%
+    call emcmake cmake -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_MODE=static -S . -B %OUTPUT_DIR%
 ) else (
-    cmake -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_C_COMPILER=clang -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_MODE=%UTILS_BUILD_MODE% -S . -B built/%UTILS_BUILD_MODE%/%BUILD_TYPE%
+    cmake -D CMAKE_CXX_COMPILER=%CXX_COMPILER% -D CMAKE_C_COMPILER=%C_COMPILER% -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% -D CMAKE_INSTALL_PREFIX=%INSTALL_PREFIX% -D UTILS_BUILD_MODE=%UTILS_BUILD_MODE% -S . -B %OUTPUT_DIR%
 )
 rem ) 
 rem else (
@@ -50,8 +71,8 @@ rem     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Comm
 rem     call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 rem     cmake -D CMAKE_CXX_COMPILER=cl -G Ninja -D CMAKE_BUILD_TYPE=%BUILD_TYPE% .
 rem )
-ninja -C built/%UTILS_BUILD_MODE%/%BUILD_TYPE% %TARGET%
+ninja -C %OUTPUT_DIR% %TARGET%
 if not %ERRORLEVEL% == 0 (
     exit /b
 )
-ninja -C built/%UTILS_BUILD_MODE%/%BUILD_TYPE% install
+ninja -C %OUTPUT_DIR% install
