816c3b79baf05dfa2e2f887bcc56ae65e8447ce1
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-04-22 14:07:53 +0900

    update

--- src/include/number/hex/hex2bin.h
+++ src/include/number/hex/hex2bin.h
@@ -15,45 +15,47 @@
 #include <binary/writer.h>
 
 namespace futils::number::hex {
-    using namespace futils::comb2;
-    using namespace ops;
-    namespace cps = composite;
+    namespace parser {
+        using namespace futils::comb2;
+        using namespace ops;
+        namespace cps = composite;
 
-    constexpr auto comment = cps::shell_comment;
-    constexpr auto hex = cps::hex;
-    constexpr auto space = cps::space | cps::tab | cps::eol;
+        constexpr auto comment = cps::shell_comment;
+        constexpr auto hex = cps::hex;
+        constexpr auto space = cps::space | cps::tab | cps::eol;
 
-    constexpr auto hex_tag = "hex";
+        constexpr auto hex_tag = "hex";
 
-    constexpr auto text = *(comment | space | str(hex_tag, hex)) & eos;
+        constexpr auto text = *(comment | space | str(hex_tag, hex)) & eos;
 
-    namespace test {
-        constexpr auto check_hex() {
-            auto seq = futils::make_ref_seq("1234567890abcdefABCDEF # comment");
-            return text(seq, 0, 0) == Status::match;
-        }
+        namespace test {
+            constexpr auto check_hex() {
+                auto seq = futils::make_ref_seq("1234567890abcdefABCDEF # comment");
+                return text(seq, 0, 0) == Status::match;
+            }
 
-        static_assert(check_hex());
-    }  // namespace test
+            static_assert(check_hex());
+        }  // namespace test
 
-    template <class Buf>
-    struct HexReader : LexContext<> {
-        Buf buf;
-        void end_string(Status res, auto&& tag, auto&& seq, Pos pos) {
-            LexContext::end_string(res, tag, seq, pos);
-            if (res == Status::match) {
-                for (auto i = pos.begin; i < pos.end; i++) {
-                    buf.push_back(seq.buf.buffer[i]);
+        template <class Buf>
+        struct HexReader : LexContext<> {
+            Buf buf;
+            void end_string(Status res, auto&& tag, auto&& seq, Pos pos) {
+                LexContext::end_string(res, tag, seq, pos);
+                if (res == Status::match) {
+                    for (auto i = pos.begin; i < pos.end; i++) {
+                        buf.push_back(seq.buf.buffer[i]);
+                    }
                 }
             }
-        }
-    };
+        };
+    }  // namespace parser
 
     template <class B, class T>
     bool read_hex(B& data, T&& input) {
         auto seq = futils::make_ref_seq(input);
-        auto ctx = HexReader<B>{};
-        if (auto res = text(seq, ctx, 0); res == Status::match) {
+        auto ctx = parser::HexReader<B>{};
+        if (auto res = parser::text(seq, ctx, 0); res == parser::Status::match) {
             if (ctx.buf.size() % 2 == 0) {
                 for (auto i = 0; i < ctx.buf.size(); i += 2) {
                     futils::byte c = ctx.buf[i];
@@ -91,9 +93,9 @@ namespace futils::number::hex {
     template <class B, class TmpB = B, class T>
     bool read_hex_stream(B& data, T&& new_data, remain_buffer& remain_buffer) {
         auto c = futils::view::make_concat(remain_buffer, new_data);
-        auto ctx = HexReader<TmpB>{};
+        auto ctx = parser::HexReader<TmpB>{};
         auto seq = futils::make_ref_seq(c);
-        if (auto res = text(seq, ctx, 0); res == Status::match) {
+        if (auto res = parser::text(seq, ctx, 0); res == parser::Status::match) {
             for (auto i = 0; i < ctx.buf.size() / 2; i++) {
                 futils::byte c = ctx.buf[i << 1];
                 futils::byte d = ctx.buf[(i << 1) + 1];
