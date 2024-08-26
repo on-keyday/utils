b934f346c2e974d0a11001b4802a52b596433e5b
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-08-26 21:46:35 +0900

    update

---
+++ src/include/number/hex/bin2hex.h
@@ -0,0 +1,30 @@
+/*
+    futils - utility library
+    Copyright (c) 2021-2024 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+#pragma once
+#include "../to_string.h"
+#include <core/sequencer.h>
+
+namespace futils::number::hex {
+
+    template <class Result>
+    constexpr void to_hex(Result& result, auto&& in) {
+        auto seq = make_ref_seq(in);
+        while (!seq.eos()) {
+            auto c = seq.current();
+            result.push_back(to_num_char(c >> 4));
+            result.push_back(to_num_char(c & 0xf));
+        }
+    }
+
+    template <class Result>
+    constexpr Result to_hex(auto&& in) {
+        Result result{};
+        to_hex(result, in);
+        return result;
+    }
+}  // namespace futils::number::hex
