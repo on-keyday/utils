203658a0d31be51c10768ce7856544c54eb019f2
on-keyday <62627905+on-keyday@users.noreply.github.com>
2024-11-30 07:58:25 +0900

    update

--- src/include/wrap/iostream.h
+++ src/include/wrap/iostream.h
@@ -15,31 +15,35 @@ namespace futils::wrap {
     template <class C>
     struct iostream_adapter {
         static constexpr binary::ReadStreamHandlerT<C, std::istream> in{
-            .empty = [](void* ctx, size_t offset) -> bool {
-                auto self = static_cast<std::istream*>(ctx);
-                return self->eof() || self->fail();
-            },
-            .direct_read = [](void* ctx, view::basic_wvec<C> w, size_t expected) -> size_t {
-                auto self = static_cast<std::istream*>(ctx);
-                self->read(reinterpret_cast<char*>(w.data()), w.size());
-                return self->gcount();
+            .base = {
+                .empty = [](void* ctx, size_t offset) -> bool {
+                    auto self = static_cast<std::istream*>(ctx);
+                    return self->eof() || self->fail();
+                },
+                .direct_read = [](void* ctx, view::basic_wvec<C> w, size_t expected) -> size_t {
+                    auto self = static_cast<std::istream*>(ctx);
+                    self->read(reinterpret_cast<char*>(w.data()), w.size());
+                    return self->gcount();
+                },
             },
         };
 
         static constexpr binary::WriteStreamHandlerT<C, std::ostream> out{
-            .full = [](void* ctx, size_t offset) -> bool {
-                auto self = static_cast<std::ostream*>(ctx);
-                return self->fail();
+            .base = {
+                .full = [](void* ctx, size_t offset) -> bool {
+                    auto self = static_cast<std::ostream*>(ctx);
+                    return self->fail();
+                },
+                .direct_write = [](void* ctx, view::basic_rvec<C> r, size_t times) {
+                    auto self = static_cast<std::ostream*>(ctx);
+                    for (size_t i = 0; i < times; i++) {
+                        self->write(reinterpret_cast<const char*>(r.data()), r.size());
+                        if(self->fail()) {
+                            return false;
+                        }
+                    } 
+                    return true; },
             },
-            .direct_write = [](void* ctx, view::basic_rvec<C> r, size_t times) {
-                auto self = static_cast<std::ostream*>(ctx);
-                for (size_t i = 0; i < times; i++) {
-                    self->write(reinterpret_cast<const char*>(r.data()), r.size());
-                    if(self->fail()) {
-                        return false;
-                    }
-                } 
-                return true; },
         };
     };
 }  // namespace futils::wrap
