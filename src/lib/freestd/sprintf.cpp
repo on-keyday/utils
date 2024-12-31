9f62b69032ed0869532c10deb7f1a677622c251b
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-01-01 00:03:09 +0900

    update license year

--- src/lib/freestd/sprintf.cpp
+++ src/lib/freestd/sprintf.cpp
@@ -1,7 +1,7 @@
 
 /*
     futils - utility library
-    Copyright (c) 2021-2024 on-keyday (https://github.com/on-keyday)
+    Copyright (c) 2021-2025 on-keyday (https://github.com/on-keyday)
     Released under the MIT license
     https://opensource.org/licenses/mit-license.php
 */
