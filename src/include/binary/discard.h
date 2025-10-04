318226b1dde17d04e1bb58762f984f0ae547df07
on-keyday <62627905+on-keyday@users.noreply.github.com>
2025-10-04 17:43:36 +0900

    add discard

---
+++ src/include/binary/discard.h
@@ -0,0 +1,26 @@
+/*
+    futils - utility library
+    Copyright (c) 2021-2025 on-keyday (https://github.com/on-keyday)
+    Released under the MIT license
+    https://opensource.org/licenses/mit-license.php
+*/
+
+#pragma once
+#include "writer.h"
+
+namespace futils::binary {
+    template <class C = byte>
+    constexpr WriteStreamHandlerT<C, size_t> discard{
+        .base = {
+            .full = [](void*, size_t) { return false; },
+            .direct_write =
+                [](void* ptr, view::basic_rvec<C> n, size_t times) {
+                    if (ptr) {
+                        size_t* count = static_cast<size_t*>(ptr);
+                        *count += n.size() * times;
+                    }
+                    return true;
+                },
+        },
+    };
+}  // namespace futils::binary
